8ac01c964d180c6983242b218ae78371b462cffc
david <david.marcinkovic@fer.hr>
2020-03-28 00:15:24 +0100

    Added Gui shader and renderer

---
+++ engine/gui/GuiRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 28. 03. 2020..
+//
+
+#ifndef SURVIVE_GUIRENDERER_H
+#define SURVIVE_GUIRENDERER_H
+
+
+class GuiRenderer
+{
+
+};
+
+
+#endif //SURVIVE_GUIRENDERER_H
---
+++ engine/gui/GuiShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 28. 03. 2020..
+//
+
+#ifndef SURVIVE_GUISHADER_H
+#define SURVIVE_GUISHADER_H
+
+
+class GuiShader
+{
+
+};
+
+
+#endif //SURVIVE_GUISHADER_H
