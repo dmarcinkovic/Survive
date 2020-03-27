3d0afa48fa2298de6314342e65417ea2d7ae751d
david <david.marcinkovic@fer.hr>
2020-03-28 00:11:50 +0100

    Created basic 2D Renderer

---
+++ engine/entity/Entity2D.cpp
@@ -0,0 +1,11 @@
+//
+// Created by david on 27. 03. 2020..
+//
+
+#include "Entity2D.h"
+
+Entity2D::Entity2D(const Texture &texture)
+    : m_Texture(texture)
+{
+
+}
---
+++ engine/renderer/Renderer2D.cpp
@@ -0,0 +1,42 @@
+//
+// Created by david on 27. 03. 2020..
+//
+
+#include "Renderer2D.h"
+
+Renderer2D::Renderer2D(const Shader &shader)
+        : m_Shader(shader)
+{
+
+}
+
+void Renderer2D::render(const std::vector<Entity2D> &entities)
+{
+    for (auto const &entity2D : entities)
+    {
+        prepareRendering(entity2D);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        finishRendering();
+    }
+}
+
+void Renderer2D::prepareRendering(const Entity2D &entity2D)
+{
+    m_Shader.start();
+    entity2D.m_Texture.bindTexture();
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+}
+
+void Renderer2D::finishRendering()
+{
+    Shader::stop();
+    Texture::unbindTexture();
+
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+    Loader::unbindVao();
+}
