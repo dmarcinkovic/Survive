1d8f06b57bec9464b949bc496e3a87bbf03d1a87
david <david.marcinkovic@fer.hr>
2020-03-28 21:46:51 +0100

    Added two entites to check if everything works

--- engine/entity/Entity2D.cpp
+++ engine/entity/Entity2D.cpp
@@ -4,8 +4,8 @@
 
 #include "Entity2D.h"
 
-Entity2D::Entity2D(const Texture &texture)
-    : m_Texture(texture)
+Entity2D::Entity2D(const Texture &texture, const glm::vec3 &position, float scale)
+        : m_Texture(texture), position(position), scale(scale)
 {
 
 }
--- engine/entity/Entity2D.h
+++ engine/entity/Entity2D.h
@@ -5,14 +5,17 @@
 #ifndef SURVIVE_ENTITY2D_H
 #define SURVIVE_ENTITY2D_H
 
-
 #include "../texture/Texture.h"
+
+#include <glm/vec3.hpp>
 
 struct Entity2D
 {
     Texture m_Texture;
+    glm::vec3 position;
+    float scale;
 
-    explicit Entity2D(const Texture &texture);
+    Entity2D(const Texture &texture, const glm::vec3 &position, float scale);
 };
 
 
