aa32f968c5036d21190d66ac80d4ad7d57f58f27
david <david.marcinkovic@fer.hr>
2020-05-03 21:43:00 +0200

    Created rounded edges on button rectangle

--- engine/shader/sources/ButtonVertexShader.glsl
+++ engine/shader/sources/ButtonVertexShader.glsl
@@ -1,10 +1,13 @@
 #version 450 core
 
 in vec3 position;
+out vec3 pos;
 
 uniform mat4 transformationMatrix;
 
 void main()
 {
     gl_Position = transformationMatrix * vec4(position, 1.0);
+
+    pos = position;
 }
--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
     Renderer2D renderer(loader);
 
     Texture texture(renderer.getModel(), 0);
-    Button button(texture, glm::vec3{0, 0, 0}, 0.9, 0.5);
+    Button button(texture, glm::vec3{0, 0, 0}, 0.5, 0.5);
 
     renderer.addButton(button);
 
