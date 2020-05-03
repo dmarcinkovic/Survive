62c6fdc16f160e1d0f8c895a8b1a570e3da63897
david <david.marcinkovic@fer.hr>
2020-05-03 23:26:30 +0200

    Created nice looking button

--- engine/shader/sources/ButtonFragmentShader.glsl
+++ engine/shader/sources/ButtonFragmentShader.glsl
@@ -5,17 +5,18 @@ in vec3 pos;
 
 uniform vec4 color;
 
-const float radius = 0.08;
+const float radius = 0.12;
 
 vec3 position = (pos + 1) / 2.0;
+float alpha = color.a - 0.9 * clamp(position.x, 0, color.a);
 
 void colorEdges(float x, float y)
 {
-    const vec3 edgePosition = abs(vec3(x, y, position.z) - position);
+    const vec3 edgePosition = vec3(x, y, position.z) - position;
 
     if (length(edgePosition) < radius)
     {
-        outColor = vec4(color);
+        outColor = vec4(color.rgb, alpha);
     } else
     {
         outColor = vec4(0);
@@ -38,6 +39,6 @@ void main()
         colorEdges(1 - radius, 1 - radius);
     } else
     {
-        outColor = vec4(color);
+        outColor = vec4(color.rgb, alpha);
     }
 }
--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ int main()
     Renderer2D renderer(loader);
 
     Texture texture(renderer.getModel(), 0);
-    Button button(texture, glm::vec3{0, 0, 0}, 0.5, 0.5,
-                  glm::vec4{1, 0, 0, 0.7});
+    Button button(texture, glm::vec3{0, 0, 0}, 0.4, 0.1,
+                  glm::vec4{1, 0, 0, 0.9});
 
     renderer.addButton(button);
 
