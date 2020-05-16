18e074e976e191b357a73d5cb56d2e657dfcef68
david <david.marcinkovic@fer.hr>
2020-05-16 17:09:50 +0200

    First commit in 3d rendering

--- main.cpp
+++ main.cpp
@@ -12,23 +12,11 @@ int main()
     Display display(width, height, "Survive");
 
     Loader loader;
-    Renderer2D renderer(loader);
 
-    Texture texture(renderer.getModel(), 1);
-
-    Button button(texture, glm::vec3{0, 0.5, 0}, 0.4, 0.2, glm::vec4{1, 0, 0, 1});
-    Button button2(texture, glm::vec3{0, 0, 0}, 0.3, 0.1, glm::vec4{0, 1, 0, 1});
-    Button button3(texture, glm::vec3{-0.2, -0.4, 0}, 0.7, 0.15, glm::vec4{0, 0, 1, 1});
-
-    renderer.addButton(button);
-    renderer.addButton(button2);
-    renderer.addButton(button3);
-
+        
     while (display.isRunning())
     {
         Display::clearWindow();
-
-        renderer.render();
 
         display.update();
     }
