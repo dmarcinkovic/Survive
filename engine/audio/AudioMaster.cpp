f9eba8c73459b14025581512b0875a6ca2c346c1
david <david.marcinkovic@fer.hr>
2020-06-13 01:48:32 +0200

    Working on loading sound

--- engine/audio/AudioMaster.cpp
+++ engine/audio/AudioMaster.cpp
@@ -2,4 +2,39 @@
 // Created by david on 13. 06. 2020..
 //
 
+#include <cstdint>
 #include "AudioMaster.h"
+
+ALuint AudioMaster::loadSound(const char *filename)
+{
+    m_Device = alcOpenDevice(nullptr);
+    m_Context = alcCreateContext(m_Device, nullptr);
+    alcMakeContextCurrent(m_Context);
+
+    ALuint buffer;
+    alGenBuffers(1, &buffer);
+
+    m_Buffers.emplace_back(buffer);
+
+    std::uint8_t channels;
+    std::int32_t sampleRate;
+    std::uint8_t bitsPerSample;
+    ALsizei size;
+
+    const char *wavFile = loadWav(filename, channels, sampleRate, bitsPerSample, size);
+
+    
+
+    return 0;
+}
+
+AudioMaster::~AudioMaster()
+{
+    alDeleteBuffers(m_Buffers.size(), m_Buffers.data());
+}
+
+char *AudioMaster::loadWav(const char *filename, uint8_t &channels, int32_t &sampleRate, uint8_t &bitsPerSample,
+                           ALsizei &size)
+{
+
+}
--- engine/audio/AudioMaster.h
+++ engine/audio/AudioMaster.h
@@ -5,9 +5,26 @@
 #ifndef SURVIVE_AUDIOMASTER_H
 #define SURVIVE_AUDIOMASTER_H
 
+#include <AL/alc.h>
+#include <AL/al.h>
+#include <vector>
 
 class AudioMaster
 {
+private:
+    ALCdevice *m_Device;
+    ALCcontext *m_Context;
+
+    std::vector<ALuint> m_Buffers;
+
+public:
+    ALuint loadSound(const char *filename);
+
+    ~AudioMaster();
+
+private:
+    static char *loadWav(const char *filename, std::uint8_t &channels,
+                         std::int32_t &sampleRate, std::uint8_t &bitsPerSample, ALsizei &size);
 
 };
 
