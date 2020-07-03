274cd82d631e5576e923a92f19582a066596bc40
david <david.marcinkovic@fer.hr>
2020-07-03 18:38:44 +0200

    Implemented collision between static and dynamic circle

--- engine/physics/collision/Circle.cpp
+++ engine/physics/collision/Circle.cpp
@@ -13,18 +13,11 @@ Circle::Circle(Entity2D &circle, float radius, float mass, const BodyType &bodyT
 
 void Circle::collide(Circle &circle)
 {
-    if (m_BodyType == BodyType::STATIC)
-    {
-        std::cout << "error : this should not happen\n";
-        return;
-    }
-
     auto position = m_Body.m_Position - circle.m_Body.m_Position;
     float totalDistance = m_Radius + circle.m_Radius;
 
     if (position.x * position.x + position.y * position.y > totalDistance * totalDistance)
     {
-//        std::cout << "No collision\n";
         return;
     }
 
@@ -44,6 +37,19 @@ void Circle::collide(Circle &circle)
 
         m_Velocity = k1 * velocity + k2 * circle.m_Velocity;
         circle.m_Velocity = k2 * circle.m_Velocity + k1 * velocity;
+
+        glm::vec2 connection = m_Body.m_Position - circle.m_Body.m_Position;
+        float len = glm::length(connection);
+
+        glm::vec2 intersectionPoint = (m_Radius / len) * circle.m_Body.m_Position +
+                                      (circle.m_Radius / len) * m_Body.m_Position;
+
+        glm::vec2 vec1 = glm::vec2(m_Body.m_Position) - intersectionPoint;
+        glm::vec2 vec2 = glm::vec2(circle.m_Body.m_Position) - intersectionPoint;
+        vec1 /= 40;
+        vec2 /= 10;
+        m_Velocity += vec1;
+        circle.m_Velocity += vec2;
     } else
     {
         glm::vec2 normal = m_Body.m_Position - circle.m_Body.m_Position;
--- main.cpp
+++ main.cpp
@@ -16,23 +16,18 @@ int main()
     Loader loader;
     Renderer2D renderer(loader);
 
-    Texture texture(renderer.getModel(), loader.loadTexture("res/circle.png"));
-    Texture texture1(renderer.getModel(), loader.loadTexture("res/circle.png"));
+    Texture circleTexture(renderer.getModel(), loader.loadTexture("res/circle.png"));
+    Texture rectangleTexture(renderer.getModel(), loader.loadTexture("res/rectangle.png"));
 
-    Entity2D entity1(texture, glm::vec3{-0.5, 0.5, 0.0}, 0.3);
-    Entity2D entity2(texture, glm::vec3{0.5, 0.25, 0.0}, 0.1);
-    Entity2D entity3(texture1, glm::vec3{-0.5, -0.5, 0.0}, 0.3);
+    Entity2D circle(circleTexture, glm::vec3{0.5, 0.25, 0.0}, 0.1);
+    Entity2D rectangle(rectangleTexture, glm::vec3{-0.5, -0.5, 0.0}, 0.3);
 
     World world;
-    world.addBody(std::make_unique<Circle>(entity1, 0.3, 5, BodyType::STATIC));
-    world.addBody(std::make_unique<Circle>(entity2, 0.1, 1, BodyType::DYNAMIC, glm::vec2{-0.01, 0}));
-//    world.addBody(std::make_unique<Rectangle>(entity3, 0.2, 0.4, 1, BodyType::STATIC));
+    world.addBody(std::make_unique<Circle>(circle, 0.1, 10, BodyType::DYNAMIC, glm::vec2{-0.01, 0}));
+    world.addBody(std::make_unique<Rectangle>(rectangle, 0.2, 0.4, 1, BodyType::STATIC));
 
-//    world.step();
-//    world.step();
-
-    renderer.addGui(entity1);
-    renderer.addGui(entity2);
+    renderer.addGui(circle);
+    renderer.addGui(rectangle);
 
     while (display.isRunning())
     {
