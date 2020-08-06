a8da378561876ff2bdef4b5c281bc07435e61df3
david <david.marcinkovic@fer.hr>
2020-08-06 21:38:33 +0200

    Created class for calculating shadow box

---
+++ engine/shadowMapping/ShadowBox.cpp
@@ -0,0 +1,10 @@
+//
+// Created by david on 06. 08. 2020..
+//
+
+#include "ShadowBox.h"
+
+void ShadowBox::calculateShadowBox()
+{
+
+}
---
+++ engine/shadowMapping/ShadowBox.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 06. 08. 2020..
+//
+
+#ifndef SURVIVE_SHADOWBOX_H
+#define SURVIVE_SHADOWBOX_H
+
+
+class ShadowBox
+{
+public:
+    static void calculateShadowBox();
+};
+
+
+#endif //SURVIVE_SHADOWBOX_H
