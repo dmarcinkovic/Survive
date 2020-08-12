d64657e1a4b32c5e3650c24ec5f561758e253c54
david <david.marcinkovic@fer.hr>
2020-08-12 15:25:10 +0200

    Wokring on animation fragment shader

--- engine/shader/sources/AnimationFragmentShader.glsl
+++ engine/shader/sources/AnimationFragmentShader.glsl
@@ -4,7 +4,21 @@ in vec3 surfaceNormal;
 in vec2 texCoordinates;
 in vec3 worldPosition;
 
+uniform sampler2D objectTexture;
+uniform vec3 lightPosition;
+uniform vec3 lightColor;
+
+out vec4 outColor;
+
 void main()
 {
+    vec3 lightPos = normalize(lightPosition);
+    vec4 textureColor = texture(objectTexture, texCoordinates);
 
+    const float ambientFactor = 0.2;
+    vec3 ambient = lightColor * ambientFactor;
+
+    vec3 lightDirection = normalize(lightPos - worldPosition);
+    const float diffuseFactor = max(dot(lightDirection, surfaceNormal), 0.0);
+    vec3 diffuse = lightColor * diffuseFactor;
 }
