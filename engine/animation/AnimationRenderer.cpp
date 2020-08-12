16b63e7954d86151cf714baa16d3097339a10892
david <david.marcinkovic@fer.hr>
2020-08-12 14:24:57 +0200

    Created class animation renderer

---
+++ engine/animation/AnimationRenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 12. 08. 2020..
+//
+
+#include "AnimationRenderer.h"
---
+++ engine/animation/AnimationRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 12. 08. 2020..
+//
+
+#ifndef SURVIVE_ANIMATIONRENDERER_H
+#define SURVIVE_ANIMATIONRENDERER_H
+
+
+class AnimationRenderer
+{
+
+};
+
+
+#endif //SURVIVE_ANIMATIONRENDERER_H
