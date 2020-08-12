5f7b1c28e3f91605fd45a1d5f77595c84cae4a5d
david <david.marcinkovic@fer.hr>
2020-08-12 14:25:21 +0200

    Created class animation shader

---
+++ engine/animation/AnimationShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 12. 08. 2020..
+//
+
+#ifndef SURVIVE_ANIMATIONSHADER_H
+#define SURVIVE_ANIMATIONSHADER_H
+
+
+class AnimationShader
+{
+
+};
+
+
+#endif //SURVIVE_ANIMATIONSHADER_H
