b9bdadc65e2f97f0860e93e2160a12a9da6a060c
david <david.marcinkovic@fer.hr>
2020-08-13 15:31:49 +0200

    Working on animation renderer

--- engine/animation/AnimationRenderer.cpp
+++ engine/animation/AnimationRenderer.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "AnimationRenderer.h"
+#include "../math/Maths.h"
+
+AnimationRenderer::AnimationRenderer(const Light& light)
+    : m_Light(light)
+{
+    m_Shader.start();
+
+    auto projectionMatrix = Maths::createProjectionMatrix(fieldOfView, near, far);
+    m_Shader.loadProjectionMatrix(projectionMatrix);
+
+    AnimationShader::stop();
+}
+
+void AnimationRenderer::render() const
+{
+
+}
+
+void AnimationRenderer::addObject(Object3D &object)
+{
+    m_Objects.emplace_back(object);
+}
--- engine/animation/AnimationRenderer.h
+++ engine/animation/AnimationRenderer.h
@@ -5,10 +5,31 @@
 #ifndef SURVIVE_ANIMATIONRENDERER_H
 #define SURVIVE_ANIMATIONRENDERER_H
 
+#include <vector>
+#include <functional>
+
+#include "AnimationShader.h"
+#include "../light/Light.h"
+#include "../objects/Object3D.h"
 
 class AnimationRenderer
 {
+private:
+    constexpr static const float fieldOfView = 70.0f;
+    constexpr static const float near = 0.1f;
+    constexpr static const float far = 1000.0f;
 
+    AnimationShader m_Shader;
+
+    std::vector<std::reference_wrapper<Object3D>> m_Objects;
+    const Light &m_Light;
+
+public:
+    explicit AnimationRenderer(const Light &light);
+
+    void render() const;
+
+    void addObject(Object3D &object);
 };
 
 
