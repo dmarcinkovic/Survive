439a217dbec48df3817a02bc3787c70c62952ec2
david <david.marcinkovic@fer.hr>
2020-08-23 14:24:58 +0200

    Added location

--- engine/shader/sources/ButtonVertexShader.glsl
+++ engine/shader/sources/ButtonVertexShader.glsl
@@ -1,10 +1,10 @@
 #version 450 core
 
-in vec3 position;
-out vec3 pos;
+layout (location = 0) in vec3 position;
 
 uniform mat4 transformationMatrix;
 
+out vec3 pos;
 out vec2 textureCoordinates;
 
 void main()
