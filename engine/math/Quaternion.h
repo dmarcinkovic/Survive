5c0df37fa970d218bb00188786cd531f2a1d3d83
david <david.marcinkovic@fer.hr>
2020-09-05 16:48:18 +0200

    Done with basic operation for Quaternion class

--- engine/math/Quaternion.h
+++ engine/math/Quaternion.h
@@ -5,9 +5,23 @@
 #ifndef SURVIVE_QUATERNION_H
 #define SURVIVE_QUATERNION_H
 
+#include <glm/glm.hpp>
 
 class Quaternion
 {
+private:
+    float m_X, m_Y, m_Z, m_W;
+
+public:
+    Quaternion(float x, float y, float z, float w);
+
+    void normalize();
+
+    glm::mat4 toRotationMatrix() const;
+
+    static Quaternion fromMatrix(const glm::mat4 &matrix);
+
+    static Quaternion interpolate(const Quaternion &a, const Quaternion &b, float blend);
 
 };
 
