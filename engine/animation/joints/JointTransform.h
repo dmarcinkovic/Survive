87246eb6bd72152add047ae92744a35f6523543a
david <david.marcinkovic@fer.hr>
2020-09-06 00:49:45 +0200

    Made joint transform clas

---
+++ engine/animation/joints/JointTransform.h
@@ -0,0 +1,28 @@
+//
+// Created by david on 06. 09. 2020..
+//
+
+#ifndef SURVIVE_JOINTTRANSFORM_H
+#define SURVIVE_JOINTTRANSFORM_H
+
+#include <glm/glm.hpp>
+#include "../../math/Quaternion.h"
+
+class JointTransform
+{
+private:
+    glm::vec3 m_Position;
+    Quaternion m_Rotation;
+
+public:
+    JointTransform(const glm::vec3 &position, const Quaternion &quaternion);
+
+    [[nodiscard]] glm::mat4 getLocalTransform() const;
+
+    static JointTransform interpolate(const JointTransform &frameA, const JointTransform &frameB, float progression);
+
+    static glm::vec3 interpolate(const glm::vec3 &start, const glm::vec3 &end, float progression);
+};
+
+
+#endif //SURVIVE_JOINTTRANSFORM_H
