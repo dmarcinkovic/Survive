38ce6c6b54724ef8f18104ef0c17ef2f235a678a
david <david.marcinkovic@fer.hr>
2020-10-03 10:17:29 +0200

    Fixed bugs with animated model

--- engine/animations/animation/AnimatedModel.cpp
+++ engine/animations/animation/AnimatedModel.cpp
@@ -6,11 +6,11 @@
 
 #include <utility>
 
-AnimatedModel::AnimatedModel(Joint rootJoint, int jointCount, const Animation &animation, const TexturedModel &texture,
+AnimatedModel::AnimatedModel(Joint rootJoint, int jointCount, const TexturedModel &texture,
                              const glm::vec3 &position, const glm::vec3 &rotation, bool isTransparent, float scaleX,
                              float scaleY, float scaleZ)
         : Object3D(texture, position, rotation, isTransparent, scaleX, scaleY, scaleZ),
-          m_RootJoint(std::move(rootJoint)), m_JointCount(jointCount), m_Animator(animation, *this)
+          m_RootJoint(std::move(rootJoint)), m_JointCount(jointCount)
 {
     m_RootJoint.calculateInverseBindTransform(glm::mat4{1});
 }
--- engine/animations/animation/AnimatedModel.h
+++ engine/animations/animation/AnimatedModel.h
@@ -10,19 +10,15 @@
 #include "../joints/Joint.h"
 #include "../../entity/Entity.h"
 #include "../../objects/Object3D.h"
-#include "Animator.h"
-
-class Animator;
 
 class AnimatedModel : public Object3D
 {
 private:
     Joint m_RootJoint;
     int m_JointCount;
-    Animator m_Animator;
 
 public:
-    AnimatedModel(Joint rootJoint, int jointCount, const Animation &animation, const TexturedModel &texture, const glm::vec3 &position,
+    AnimatedModel(Joint rootJoint, int jointCount, const TexturedModel &texture, const glm::vec3 &position,
                   const glm::vec3 &rotation = glm::vec3{0, 0, 0}, bool isTransparent = false,
                   float scaleX = 1.0f, float scaleY = 1.0f, float scaleZ = 1.0f);
 
