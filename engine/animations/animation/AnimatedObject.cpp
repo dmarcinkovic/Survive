59861783d9579f4df18f41f55dc7803cdb10975a
david <david.marcinkovic@fer.hr>
2020-10-09 00:51:19 +0200

    Fixed coping joints

--- engine/animations/animation/AnimatedObject.cpp
+++ engine/animations/animation/AnimatedObject.cpp
@@ -4,13 +4,13 @@
 
 #include "AnimatedObject.h"
 
-#include <utility>
+#include <iostream>
 
-AnimatedObject::AnimatedObject(Joint rootJoint, int numberOfJoints, const TexturedModel &texture,
+AnimatedObject::AnimatedObject(Joint &rootJoint, int numberOfJoints, const TexturedModel &texture,
                                const glm::vec3 &position, const glm::vec3 &rotation, bool isTransparent, float scaleX,
                                float scaleY, float scaleZ)
         : Object3D(texture, position, rotation, isTransparent, scaleX, scaleY, scaleZ),
-          m_RootJoint(std::move(rootJoint)), m_NumberOfJoints(numberOfJoints)
+          m_RootJoint(rootJoint), m_NumberOfJoints(numberOfJoints)
 {
     m_RootJoint.calculateInverseBindTransform(glm::mat4{1});
 }
@@ -25,13 +25,27 @@ std::vector<glm::mat4> AnimatedObject::getJointTransforms() const
     std::vector<glm::mat4> jointMatrices(m_NumberOfJoints);
     addJointsToArray(m_RootJoint, jointMatrices);
 
+    for (int k = 0; k < 4; ++k)
+    {
+        for (const auto &transform : jointMatrices)
+        {
+            for (int j = 0; j < 4; ++j)
+            {
+                std::cout << transform[k][j] << ' ';
+            }
+            std::cout << "  ";
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+
     return jointMatrices;
 }
 
 void AnimatedObject::addJointsToArray(const Joint &headJoint, std::vector<glm::mat4> &jointMatrices) const
 {
     jointMatrices[headJoint.index()] = headJoint.getAnimatedTransform();
-    for (auto const& childJoint : headJoint.children())
+    for (auto const &childJoint : headJoint.children())
     {
         addJointsToArray(childJoint, jointMatrices);
     }
