b04991c14eeec8d50c0bbbc1650ca56d456a8254
david <david.marcinkovic@fer.hr>
2020-12-05 00:54:53 +0100

    Created VerticalBlurShader class

---
+++ engine/gaussianBlur/VerticalBlurShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 05. 12. 2020..
+//
+
+#ifndef SURVIVE_VERTICALBLURSHADER_H
+#define SURVIVE_VERTICALBLURSHADER_H
+
+
+class VerticalBlurShader
+{
+
+};
+
+
+#endif //SURVIVE_VERTICALBLURSHADER_H
