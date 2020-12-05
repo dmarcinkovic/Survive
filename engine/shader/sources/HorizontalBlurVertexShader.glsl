b9f80ccbbfa8c712d13706a83534d0054bc6c6b8
david <david.marcinkovic@fer.hr>
2020-12-05 01:24:58 +0100

    Done writing HorizontalBlurVertexShader

--- engine/shader/sources/HorizontalBlurVertexShader.glsl
+++ engine/shader/sources/HorizontalBlurVertexShader.glsl
@@ -1,5 +1,20 @@
-#version 120
+#version 450 core
 
-void main() {
+in vec2 position;
 
+uniform float targetWidth;
+
+out vec2 blurTextureCoordinates[11];
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+    vec2 centerTextureCoordinates = position * 0.5 + 0.5;
+
+    float pixelSize = 1.0 / targetWidth;
+
+    for (int i = -5; i <= 5; ++i)
+    {
+        blurTextureCoordinates[i + 5] = centerTextureCoordinates + vec2(pixelSize * i, 0.0);
+    }
 }
