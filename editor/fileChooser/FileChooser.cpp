d45bc0b3f86b52e3e0f3e91332ecb709170efa7f
david <david.marcinkovic@fer.hr>
2020-12-06 16:23:49 +0100

    Show directory icon as image button

--- editor/fileChooser/FileChooser.cpp
+++ editor/fileChooser/FileChooser.cpp
@@ -17,27 +17,19 @@ void FileChooser::open()
 
 	ImGui::Begin("Open...");
 
-	ImGuiIO &io = ImGui::GetIO();
-	auto my_tex_w = (float) io.Fonts->TexWidth;
-	auto my_tex_h = (float) io.Fonts->TexHeight;
+	auto texture = reinterpret_cast<void *>(Loader::loadTexture("res/directory.png"));
+	
+	ImGui::PushID(0);
+	int frame_padding = -1;
+	ImVec2 size = ImVec2(60.0f, 60.0f);
+	ImVec2 uv0 = ImVec2(0.0f, 0.0f);
+	ImVec2 uv1 = ImVec2(1.0f, 1.0f);
+	ImVec4 bg_col = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+	ImVec4 tint_col = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+	ImGui::ImageButton(texture, size, uv0, uv1, frame_padding, bg_col, tint_col);
+	ImGui::PopID();
+	ImGui::SameLine();
 
-	auto texture = reinterpret_cast<void*>(Loader::loadTexture("res/rock.png"));
-
-	static int pressed_count = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		ImGui::PushID(i);
-		int frame_padding = -1 + i;
-		ImVec2 size = ImVec2(32.0f, 32.0f);
-		ImVec2 uv0 = ImVec2(0.0f, 0.0f);
-		ImVec2 uv1 = ImVec2(32.0f / my_tex_w, 32.0f / my_tex_h);
-		ImVec4 bg_col = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
-		ImVec4 tint_col = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-		if (ImGui::ImageButton(texture, size, uv0, uv1, frame_padding, bg_col, tint_col))
-			pressed_count += 1;
-		ImGui::PopID();
-		ImGui::SameLine();
-	}
 
 	ImGui::End();
 }
