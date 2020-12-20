51b3951b0a4d2090411baaf72f6a25d4fe5702ad
david <david.marcinkovic@fer.hr>
2020-12-20 22:32:28 +0100

    Added unform variable called projectionMatrix in GuiVertexShader

--- engine/shader/sources/GuiVertexShader.glsl
+++ engine/shader/sources/GuiVertexShader.glsl
@@ -5,9 +5,10 @@ layout (location = 0) in vec2 position;
 out vec2 textureCoords;
 
 uniform mat4 transformationMatrix;
+uniform mat4 projectionMatrix;
 
 void main()
 {
-    gl_Position = transformationMatrix * vec4(position, 0.0, 1.0);
+    gl_Position = projectionMatrix * transformationMatrix * vec4(position, 0.0, 1.0);
     textureCoords = position / 2.0 + vec2(0.5);
 }
