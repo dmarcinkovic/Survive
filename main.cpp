76f309890746a8616645a0ac68ffc8aa464f0fa7
david <david.marcinkovic@fer.hr>
2020-12-25 17:04:14 +0100

    Clean up main.cpp and create TexturedModel for cube map

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "engine/display/Display.h"
 #include "engine/renderer/Loader.h"
-#include "engine/renderer/Renderer2D.h"
-#include "engine/physics/World.h"
+#include "engine/texture/TexturedModel.h"
 
 int main()
 {
@@ -11,27 +10,13 @@ int main()
 	Display display(width, height, "Survive");
 
 	Loader loader;
-	Renderer2D renderer(loader);
-
-	TexturedModel circleTexture(loader.renderQuad(), Loader::loadTexture("res/circle.png"));
-	TexturedModel rectangleTexture(loader.renderQuad(), Loader::loadTexture("res/rectangle.png"));
-
-	Entity circle(circleTexture, glm::vec3{0.5, 0.25, 0.0}, 0.1, 0.1);
-	Entity rectangle(rectangleTexture, glm::vec3{-0.5, 0.5, 0.0}, 0.3, 0.2);
-
-	World world;
-	world.addBody(std::make_unique<Circle>(circle, 0.1, 10, BodyType::DYNAMIC, glm::vec2{-0.01, 0}));
-	world.addBody(std::make_unique<Rectangle>(rectangle, 0.6, 0.6, 1, BodyType::STATIC));
-
-	renderer.addGui(circle);
-	renderer.addGui(rectangle);
+	TexturedModel texturedModel(loader.renderCube(), Loader::loadCubeMap(
+			{"res/right.png", "res/left.png", "res/top.png", "res/bottom.png", "res/front.png", "res/back.png"}));
 
 	while (display.isRunning())
 	{
 		Display::clearWindow();
 
-		world.step();
-		renderer.render();
 
 		display.update();
 	}
