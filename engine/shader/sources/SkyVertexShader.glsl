5e53ae93d9cd1197177d729db89fd493d6c2bd89
david <david.marcinkovic@fer.hr>
2020-12-25 19:17:57 +0100

    Done with sky vertex shader

--- engine/shader/sources/SkyVertexShader.glsl
+++ engine/shader/sources/SkyVertexShader.glsl
@@ -1,6 +1,14 @@
 #version 450 core
 
+layout (location = 0) in vec3 position;
+
+out vec3 textureCoordinates;
+
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
 void main()
 {
-
+    textureCoordinates = position;
+    gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
 }
