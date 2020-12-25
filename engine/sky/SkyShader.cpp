f4e38ef32904fcf006f3cb476c84fb8d16b06069
david <david.marcinkovic@fer.hr>
2020-12-25 18:51:55 +0100

    Created method for loading transformation, view and projection matrices into the vertex shader

--- engine/sky/SkyShader.cpp
+++ engine/sky/SkyShader.cpp
@@ -12,5 +12,18 @@ SkyShader::SkyShader()
 
 void SkyShader::getUniformLocations()
 {
+	m_LocationTransformationMatrix = glGetUniformLocation(m_Program, "transformationMatrix");
+	m_LocationProjectionMatrix = glGetUniformLocation(m_Program, "projectionMatrix");
+	m_LocationViewMatrix = glGetUniformLocation(m_Program, "viewMatrix");
+}
 
+void SkyShader::loadTransformationMatrix(const glm::mat4 &transformationMatrix) const
+{
+	loadMatrix(m_LocationTransformationMatrix, transformationMatrix);
+}
+
+void SkyShader::loadViewAndProjectionMatrices(const glm::mat4 &viewMatrix, const glm::mat4 &projectionMatrix) const
+{
+	loadMatrix(m_LocationViewMatrix, viewMatrix);
+	loadMatrix(m_LocationProjectionMatrix, projectionMatrix);
 }
