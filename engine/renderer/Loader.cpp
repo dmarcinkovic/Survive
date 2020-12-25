f5d317f0fc931dff3836850c74546652b3a21e02
david <david.marcinkovic@fer.hr>
2020-12-25 16:46:32 +0100

    Refactor code

--- engine/renderer/Loader.cpp
+++ engine/renderer/Loader.cpp
@@ -198,21 +198,11 @@ Model Loader::renderQuad()
 
 Model Loader::renderCube()
 {
-	static const std::vector<float> vertices{-1, -1, 1,
-											 1, -1, 1,
-											 -1, 1, 1,
-											 1, 1, 1,
-											 -1, 1, -1,
-											 1, 1, -1,
-											 -1, -1 - 1,
-											 1, -1, -1};
+	static const std::vector<float> vertices{-1, -1, 1, 1, -1, 1, -1, 1, 1, 1, 1, 1,
+											 -1, 1, -1, 1, 1, -1, -1, -1 - 1, 1, -1, -1};
 
-	static const std::vector<unsigned> indices{0, 1, 2, 2, 1, 3,
-											   2, 3, 4, 4, 3, 5,
-											   4, 5, 6, 6, 5, 7,
-											   6, 7, 0, 0, 7, 1,
-											   1, 7, 3, 3, 7, 5,
-											   6, 0, 4, 4, 0, 2};
+	static const std::vector<unsigned> indices{0, 1, 2, 2, 1, 3, 2, 3, 4, 4, 3, 5, 4, 5, 6, 6, 5, 7,
+											   6, 7, 0, 0, 7, 1, 1, 7, 3, 3, 7, 5, 6, 0, 4, 4, 0, 2};
 
 	return Model(loadToVao(vertices, indices, 3));
 }
