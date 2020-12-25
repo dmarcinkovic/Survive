5883d84193361cb2fdf4c0700886dac3d8e9a4ec
david <david.marcinkovic@fer.hr>
2020-12-25 15:26:24 +0100

    Created SkyRenderer class

---
+++ engine/sky/SkyRenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 25. 12. 2020..
+//
+
+#include "SkyRenderer.h"
---
+++ engine/sky/SkyRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 25. 12. 2020..
+//
+
+#ifndef SURVIVE_SKYRENDERER_H
+#define SURVIVE_SKYRENDERER_H
+
+
+class SkyRenderer
+{
+
+};
+
+
+#endif //SURVIVE_SKYRENDERER_H
