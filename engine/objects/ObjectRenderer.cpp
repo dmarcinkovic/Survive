8ba0b0d2190487ae404dfb3b48e74482223b8716
david <david.marcinkovic@fer.hr>
2020-12-26 20:00:18 +0100

    Load refraction index and refraction factor into the shader

--- engine/objects/ObjectRenderer.cpp
+++ engine/objects/ObjectRenderer.cpp
@@ -6,54 +6,55 @@
 #include "../math/Maths.h"
 
 ObjectRenderer::ObjectRenderer(const Light &light)
-        : m_Light(light)
+		: m_Light(light)
 {
 }
 
 void ObjectRenderer::render(const Camera &camera, GLuint shadowMap) const
 {
-    Renderer3DUtil::prepareRendering(m_Shader);
+	Renderer3DUtil::prepareRendering(m_Shader);
 
-    loadUniforms(camera, shadowMap);
+	loadUniforms(camera, shadowMap);
 
-    for (auto const&[texturedModel, objects] : m_Objects)
-    {
-        Renderer3DUtil::prepareEntity(texturedModel);
-        renderScene(objects, camera);
+	for (auto const&[texturedModel, objects] : m_Objects)
+	{
+		Renderer3DUtil::prepareEntity(texturedModel);
+		renderScene(objects, camera);
 
-        Renderer3DUtil::finishRenderingEntity();
-    }
+		Renderer3DUtil::finishRenderingEntity();
+	}
 
-    Renderer3DUtil::finishRendering();
+	Renderer3DUtil::finishRendering();
 }
 
 void ObjectRenderer::add3DObject(Object3D &entity)
 {
-    auto &batch = m_Objects[entity.m_Texture];
-    batch.emplace_back(entity);
+	auto &batch = m_Objects[entity.m_Texture];
+	batch.emplace_back(entity);
 }
 
 void
 ObjectRenderer::renderScene(const std::vector<std::reference_wrapper<Object3D>> &objects, const Camera &camera) const
 {
-    for (auto const &object : objects)
-    {
-        auto const &o = object.get();
-        auto rotation = camera.m_Rotation + o.m_Rotation;
+	for (auto const &object : objects)
+	{
+		auto const &o = object.get();
+		auto rotation = camera.m_Rotation + o.m_Rotation;
 
-        object.get().m_Skybox.bindCubeTexture(2);
-		m_Shader.loadReflectiveFactor(object.get().m_ReflectiveFactor);
+		o.m_Skybox.bindCubeTexture(2);
+		m_Shader.loadReflectiveFactor(o.m_ReflectiveFactor);
+		m_Shader.loadRefractionData(o.m_RefractiveIndex, o.m_RefractionFactor);
 
-        glm::mat4 modelMatrix = Maths::createTransformationMatrix(o.m_Position, o.m_ScaleX, o.m_ScaleY, o.m_ScaleZ,
-                                                                  rotation.x, rotation.y, rotation.z);
-        m_Shader.loadTransformationMatrix(modelMatrix);
-        Renderer3DUtil::addTransparency(!o.m_IsTransparent, !o.m_IsTransparent);
+		glm::mat4 modelMatrix = Maths::createTransformationMatrix(o.m_Position, o.m_ScaleX, o.m_ScaleY, o.m_ScaleZ,
+																  rotation.x, rotation.y, rotation.z);
+		m_Shader.loadTransformationMatrix(modelMatrix);
+		Renderer3DUtil::addTransparency(!o.m_IsTransparent, !o.m_IsTransparent);
 
-        glDrawArrays(GL_TRIANGLES, 0, o.m_Texture.vertexCount());
+		glDrawArrays(GL_TRIANGLES, 0, o.m_Texture.vertexCount());
 
-        Renderer3DUtil::addTransparency(o.m_IsTransparent, o.m_IsTransparent);
-        Texture::unbindCubeTexture();
-    }
+		Renderer3DUtil::addTransparency(o.m_IsTransparent, o.m_IsTransparent);
+		Texture::unbindCubeTexture();
+	}
 }
 
 void ObjectRenderer::loadUniforms(const Camera &camera, GLuint shadowMap) const
