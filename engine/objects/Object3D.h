b09da0a0e5700f532dea637f4b71bb190de03e82
david <david.marcinkovic@fer.hr>
2020-12-27 13:30:02 +0100

    Added flag in Object3D class used to determine if object should be outlined or not

--- engine/objects/Object3D.h
+++ engine/objects/Object3D.h
@@ -12,9 +12,12 @@ struct Object3D : public Entity
 	glm::vec3 m_Rotation;
 	bool m_IsTransparent;
 	Texture m_Skybox;
+
 	float m_ReflectiveFactor{};
 	float m_RefractionFactor{};
 	float m_RefractiveIndex{};
+
+	bool m_DrawOutline{};
 
 	Object3D(const TexturedModel &texture, const glm::vec3 &position,
 			 const glm::vec3 &rotation = glm::vec3{0, 0, 0},
