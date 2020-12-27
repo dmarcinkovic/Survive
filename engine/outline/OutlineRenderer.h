dcd51d7b42e7f2dd510f06bbc8c2ef1a82e2532f
david <david.marcinkovic@fer.hr>
2020-12-27 12:48:32 +0100

    Add OutlineRenderer class

---
+++ engine/outline/OutlineRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 27. 12. 2020..
+//
+
+#ifndef SURVIVE_OUTLINERENDERER_H
+#define SURVIVE_OUTLINERENDERER_H
+
+
+class OutlineRenderer
+{
+
+};
+
+
+#endif //SURVIVE_OUTLINERENDERER_H
