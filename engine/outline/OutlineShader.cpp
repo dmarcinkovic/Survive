475b6201c93c81ec804eed4d171c2712fe0bc5a6
david <david.marcinkovic@fer.hr>
2020-12-27 11:47:55 +0100

    Write function signatures in Outline shader class

--- engine/outline/OutlineShader.cpp
+++ engine/outline/OutlineShader.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "OutlineShader.h"
+
+OutlineShader::OutlineShader()
+{
+
+}
+
+void OutlineShader::loadTransformationMatrix(const glm::mat4 &transformationMatrix) const
+{
+
+}
+
+void OutlineShader::loadViewMatrix(const glm::mat4 &viewMatrix) const
+{
+
+}
+
+void OutlineShader::loadProjectionMatrix(const glm::mat4 &projectionMatrix) const
+{
+
+}
+
+void OutlineShader::loadUniformLocations()
+{
+
+}
--- engine/outline/OutlineShader.h
+++ engine/outline/OutlineShader.h
@@ -13,6 +13,22 @@ class OutlineShader : public Shader
 private:
 	constexpr static const char *VERTEX_SHADER = "engine/shader/sources/OutlineVertexShader.glsl";
 	constexpr static const char *FRAGMENT_SHADER = "engine/shader/sources/OutlineFragmentShader.glsl";
+
+	GLuint m_LocationTransformationMatrix{};
+	GLuint m_LocationProjectionMatrix{};
+	GLuint m_LocationViewMatrix{};
+
+public:
+	OutlineShader();
+
+	void loadTransformationMatrix(const glm::mat4 &transformationMatrix) const;
+
+	void loadViewMatrix(const glm::mat4 &viewMatrix) const;
+
+	void loadProjectionMatrix(const glm::mat4 &projectionMatrix) const;
+
+private:
+	void loadUniformLocations();
 };
 
 
