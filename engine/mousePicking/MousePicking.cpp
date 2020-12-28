cb0b55814dcd6a0375b1c30cf30cf72b7d08ad1b
david <david.marcinkovic@fer.hr>
2020-12-29 00:26:03 +0100

    Created MousePicking class

---
+++ engine/mousePicking/MousePicking.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 29. 12. 2020..
+//
+
+#include "MousePicking.h"
---
+++ engine/mousePicking/MousePicking.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 29. 12. 2020..
+//
+
+#ifndef SURVIVE_MOUSEPICKING_H
+#define SURVIVE_MOUSEPICKING_H
+
+
+class MousePicking
+{
+
+};
+
+
+#endif //SURVIVE_MOUSEPICKING_H
