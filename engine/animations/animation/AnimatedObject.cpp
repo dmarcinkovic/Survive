f09423cfe4052ae12c8c4b49e82a1a8a39c9d1f0
david <david.marcinkovic@fer.hr>
2020-12-30 16:27:21 +0100

    Remove unnecessary include

--- engine/animations/animation/AnimatedObject.cpp
+++ engine/animations/animation/AnimatedObject.cpp
@@ -6,31 +6,31 @@
 
 
 AnimatedObject::AnimatedObject(Joint &rootJoint, int numberOfJoints, const TexturedModel &texture,
-                               const glm::vec3 &position, const glm::vec3 &rotation, bool isTransparent, float scaleX,
-                               float scaleY, float scaleZ)
-        : Object3D(texture, position, rotation, isTransparent, scaleX, scaleY, scaleZ),
-          m_RootJoint(rootJoint), m_NumberOfJoints(numberOfJoints)
+							   const glm::vec3 &position, const glm::vec3 &rotation, bool isTransparent, float scaleX,
+							   float scaleY, float scaleZ)
+		: Object3D(texture, position, rotation, isTransparent, scaleX, scaleY, scaleZ),
+		  m_RootJoint(rootJoint), m_NumberOfJoints(numberOfJoints)
 {
-    m_RootJoint.calculateInverseBindTransform(glm::mat4{1});
+	m_RootJoint.calculateInverseBindTransform(glm::mat4{1});
 }
 
 Joint &AnimatedObject::rootJoint()
 {
-    return m_RootJoint;
+	return m_RootJoint;
 }
 
 std::vector<glm::mat4> AnimatedObject::getJointTransforms() const
 {
-    std::vector<glm::mat4> jointMatrices(m_NumberOfJoints);
-    addJointsToArray(m_RootJoint, jointMatrices);
-    return jointMatrices;
+	std::vector<glm::mat4> jointMatrices(m_NumberOfJoints);
+	addJointsToArray(m_RootJoint, jointMatrices);
+	return jointMatrices;
 }
 
 void AnimatedObject::addJointsToArray(const Joint &headJoint, std::vector<glm::mat4> &jointMatrices) const
 {
-    jointMatrices[headJoint.index()] = headJoint.getAnimatedTransform();
-    for (auto const &childJoint : headJoint.children())
-    {
-        addJointsToArray(childJoint, jointMatrices);
-    }
+	jointMatrices[headJoint.index()] = headJoint.getAnimatedTransform();
+	for (auto const &childJoint : headJoint.children())
+	{
+		addJointsToArray(childJoint, jointMatrices);
+	}
 }
--- engine/animations/joints/JointTransform.cpp
+++ engine/animations/joints/JointTransform.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <glm/gtc/matrix_transform.hpp>
-#include <iostream>
 
 #include "JointTransform.h"
 
