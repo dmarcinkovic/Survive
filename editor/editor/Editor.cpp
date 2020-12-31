33bfc230146b2beaf4045abbb72ac82f65eb8876
david <david.marcinkovic@fer.hr>
2020-12-31 18:54:31 +0100

    Created editor class

---
+++ editor/editor/Editor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 31. 12. 2020..
+//
+
+#include "Editor.h"
---
+++ editor/editor/Editor.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 31. 12. 2020..
+//
+
+#ifndef SURVIVE_EDITOR_H
+#define SURVIVE_EDITOR_H
+
+
+class Editor
+{
+
+};
+
+
+#endif //SURVIVE_EDITOR_H
