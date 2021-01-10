3fc8ac535b6d0820eb88740d8a9f40e56e085421
david <david.marcinkovic@fer.hr>
2021-01-10 10:08:34 +0100

    Add gaussian blur to bloomTexture in BloomRenderer render() method

--- engine/gaussianBlur/BloomRenderer.cpp
+++ engine/gaussianBlur/BloomRenderer.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "BloomRenderer.h"
+
+BloomRenderer::BloomRenderer(int width, int height)
+		: m_HorizontalRenderer(width, height), m_VerticalRenderer(width, height), m_Model(m_Loader.renderQuad())
+{
+
+}
+
+void BloomRenderer::render(const Texture &bloomTexture) const
+{
+	prepare();
+
+	m_HorizontalRenderer.render(bloomTexture, m_Model);
+	m_VerticalRenderer.render(m_HorizontalRenderer.getTexture(), m_Model);
+
+	finishRendering();
+}
+
+void BloomRenderer::prepare() const
+{
+	glBindVertexArray(m_Model.m_Vao);
+	glEnableVertexAttribArray(0);
+	glDisable(GL_DEPTH_TEST);
+}
+
+void BloomRenderer::finishRendering()
+{
+	glEnable(GL_DEPTH_TEST);
+	glDisableVertexAttribArray(0);
+	glBindVertexArray(0);
+}
--- engine/gaussianBlur/BloomRenderer.h
+++ engine/gaussianBlur/BloomRenderer.h
@@ -6,9 +6,27 @@
 #define SURVIVE_BLOOMRENDERER_H
 
 
+#include "horizontalBlur/HorizontalBlurRenderer.h"
+#include "verticalBlur/VerticalBlurRenderer.h"
+
 class BloomRenderer
 {
+private:
+	HorizontalBlurRenderer m_HorizontalRenderer;
+	VerticalBlurRenderer m_VerticalRenderer;
 
+	Loader m_Loader{};
+	Model m_Model;
+
+public:
+	BloomRenderer(int width, int height);
+
+	void render(const Texture &bloomTexture) const;
+
+private:
+	void prepare() const;
+
+	static void finishRendering();
 };
 
 
