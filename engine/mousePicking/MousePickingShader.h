9d60c57c6c61f1ad64fc22ddf49cc404f2aec407
david <david.marcinkovic@fer.hr>
2021-01-12 18:45:17 +0100

    Created mouse picking shader

---
+++ engine/mousePicking/MousePickingShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 12. 01. 2021..
+//
+
+#ifndef SURVIVE_MOUSEPICKINGSHADER_H
+#define SURVIVE_MOUSEPICKINGSHADER_H
+
+
+class MousePickingShader
+{
+
+};
+
+
+#endif //SURVIVE_MOUSEPICKINGSHADER_H
