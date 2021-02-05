47ac182c01ce26ba4330837f5e736488e0be4ca1
david <david.marcinkovic@fer.hr>
2021-02-05 19:36:51 +0100

    Created ParticleShader class

---
+++ engine/particles/ParticleShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 05. 02. 2021..
+//
+
+#ifndef SURVIVE_PARTICLESHADER_H
+#define SURVIVE_PARTICLESHADER_H
+
+
+class ParticleShader
+{
+
+};
+
+
+#endif //SURVIVE_PARTICLESHADER_H
