bc2a36a87b970fe503737e090abed772da28bfc9
david <david.marcinkovic@fer.hr>
2021-02-06 11:51:01 +0100

    Clean up the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
 #include "engine/display/Display.h"
 #include "engine/renderer/Loader.h"
-#include "engine/texture/TexturedModel.h"
-#include "engine/entity/Entity.h"
-#include "engine/sky/SkyRenderer.h"
 #include "engine/renderer/Renderer3D.h"
-#include "engine/parser/ObjParser.h"
 
 int main()
 {
@@ -16,37 +11,10 @@ int main()
 
 	Loader loader;
 	Camera camera;
-	TexturedModel texturedModel(loader.renderCube(), Loader::loadCubeMap(
-			{"res/right.png", "res/left.png", "res/top.png", "res/bottom.png", "res/front.png", "res/back.png"}));
-
-	Entity sky(texturedModel, glm::vec3{});
-	Light light(glm::vec3{100, 100, 100}, glm::vec3{1, 1, 1});
-	Renderer3D renderer(light);
-
-	TexturedModel dragonModel(ObjParser::loadObj("res/dragon.obj", loader), Loader::loadTexture("res/lamp.jpg"));
-	Object3D dragon(dragonModel, glm::vec3{-5, -5, -30});
-	dragon.m_Skybox = sky.m_Texture.getTexture();
-	dragon.m_ReflectiveFactor = 0.6;
-
-	Object3D dragon2(dragonModel, glm::vec3{5, -5, -30});
-	dragon2.m_Skybox = sky.m_Texture.getTexture();
-	dragon2.m_ReflectiveFactor = 0.4;
-
-	renderer.add3DObject(dragon);
-	renderer.add3DObject(dragon2);
-	renderer.addSkyboxEntity(sky);
-	renderer.addOutlineToObject(dragon);
-	renderer.addOutlineToObject(dragon2);
-
-	std::vector<std::reference_wrapper<Object3D>> objects;
-	objects.emplace_back(dragon);
-	objects.emplace_back(dragon2);
 
 	while (display.isRunning())
 	{
 		Display::clearWindow();
-
-		renderer.render(camera);
 
 		display.update();
 	}
