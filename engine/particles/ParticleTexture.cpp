05b722367a9cca6cee153746a76c85feadd069d3
david <david.marcinkovic@fer.hr>
2021-02-10 20:35:11 +0100

    Created ParticleTexture class that will contain information about particle texture atlas

---
+++ engine/particles/ParticleTexture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 10. 02. 2021..
+//
+
+#include "ParticleTexture.h"
---
+++ engine/particles/ParticleTexture.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 10. 02. 2021..
+//
+
+#ifndef SURVIVE_PARTICLETEXTURE_H
+#define SURVIVE_PARTICLETEXTURE_H
+
+
+class ParticleTexture
+{
+
+};
+
+
+#endif //SURVIVE_PARTICLETEXTURE_H
