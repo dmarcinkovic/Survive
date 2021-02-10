5af67b84810eb943af1f3d44053da3b8dbb4f0c5
david <david.marcinkovic@fer.hr>
2021-02-10 22:59:40 +0100

    Created Particle constructor and initializing all members

--- engine/particles/Particle.cpp
+++ engine/particles/Particle.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Particle.h"
+
+Particle::Particle(const TexturedModel &particleTexture, int rows, const glm::vec3 &position, const glm::vec3 &velocity,
+				   float gravityEffect, float lifeLength, float rotation, float scale)
+		: Entity(particleTexture, position, glm::vec3{scale, scale, scale}), m_Rows(rows), m_Velocity(velocity),
+		  m_GravityEffect(gravityEffect), m_LifeLength(lifeLength), m_Rotation(rotation)
+{
+
+}
+
+bool Particle::update(const Camera &camera)
+{
+	return false;
+}
--- engine/particles/Particle.h
+++ engine/particles/Particle.h
@@ -6,21 +6,35 @@
 #define SURVIVE_PARTICLE_H
 
 
+#include <glm/vec2.hpp>
 #include "../entity/Entity.h"
+#include "ParticleTexture.h"
+#include "../camera/Camera.h"
 
 class Particle : public Entity
 {
+private:
 	glm::vec3 m_Velocity{};
 	glm::vec3 m_Rotation{};
 
 	float m_BlendFactor{};
+	int m_Rows;
 
-private:
 	float m_GravityEffect{};
 	float m_LifeLength{};
 
 	float m_ElapsedTime{};
 
+	glm::vec2 m_TextureOffset1{};
+	glm::vec2 m_TextureOffset2{};
+
+	float m_Distance{};
+
+public:
+	Particle(const TexturedModel &particleTexture, int rows, const glm::vec3 &position, const glm::vec3 &velocity,
+			 float gravityEffect, float lifeLength, float rotation = 0.0f, float scale = 1.0f);
+
+	bool update(const Camera &camera);
 };
 
 
