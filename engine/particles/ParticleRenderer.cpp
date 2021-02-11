8e28d5ba1401b8058b5dcca6ccbe11291f90edb8
david <david.marcinkovic@fer.hr>
2021-02-11 20:54:15 +0100

    Enable alpha blending

--- engine/particles/ParticleRenderer.cpp
+++ engine/particles/ParticleRenderer.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "ParticleRenderer.h"
+
+void ParticleRenderer::render(const Camera &camera)
+{
+
+}
+
+void ParticleRenderer::prepare()
+{
+	m_Shader.start();
+	enableBlending();
+
+
+}
+
+void ParticleRenderer::finish()
+{
+	Shader::stop();
+
+	glDisable(GL_BLEND);
+
+}
+
+void ParticleRenderer::enableBlending()
+{
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	glDepthMask(false);
+}
--- engine/particles/ParticleRenderer.h
+++ engine/particles/ParticleRenderer.h
@@ -6,9 +6,31 @@
 #define SURVIVE_PARTICLERENDERER_H
 
 
+#include <unordered_map>
+
+#include "ParticleShader.h"
+#include "../camera/Camera.h"
+#include "../texture/TexturedModel.h"
+#include "Particle.h"
+
 class ParticleRenderer
 {
+private:
+	constexpr static int MAX_INSTANCES = 10'000;
+	int m_Pointer{};
+	std::unordered_map<TexturedModel, std::vector<Particle>>, TextureHash> m_Particles;
 
+	ParticleShader m_Shader;
+
+public:
+	void render(const Camera &camera);
+
+private:
+	void prepare();
+
+	void finish();
+
+	static void enableBlending();
 };
 
 
