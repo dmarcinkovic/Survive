7cbfeb9ae2ee1b7cad7056d160135ad2939e6fde
david <david.marcinkovic@fer.hr>
2021-02-11 20:37:45 +0100

    Created ParticleRenderer class

---
+++ engine/particles/ParticleRenderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 11. 02. 2021..
+//
+
+#include "ParticleRenderer.h"
---
+++ engine/particles/ParticleRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 11. 02. 2021..
+//
+
+#ifndef SURVIVE_PARTICLERENDERER_H
+#define SURVIVE_PARTICLERENDERER_H
+
+
+class ParticleRenderer
+{
+
+};
+
+
+#endif //SURVIVE_PARTICLERENDERER_H
