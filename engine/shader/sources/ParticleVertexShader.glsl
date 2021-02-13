dac25751629516bbb7fb68737a2d7e684968aae0
david <david.marcinkovic@fer.hr>
2021-02-13 13:35:29 +0100

    Done with Particle vertex shader

--- engine/shader/sources/ParticleVertexShader.glsl
+++ engine/shader/sources/ParticleVertexShader.glsl
@@ -1,5 +1,26 @@
-#version 120
+#version 450 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) mat4 modelViewMatrix;
+layout (location = 5) float blendFactor;
+layout (location = 6) vec4 texOffsets;
+
+out vec2 textureCoordinates1;
+out vec2 textureCoordinates2;
+out float blend;
+
+uniform mat4 projectionMatrix;
+uniform int numberOfRows;
 
 void main() {
+    vec2 textureCoordinates = position.xy + vec2(0.5, 0.5);
 
+    textureCoordinates.y = 1.0 - textureCoordinates.y;
+    textureCoordinates /= float(numberOfRows);
+
+    textureCoordinates1 = textureCoordinates + texOffsets.xy;
+    textureCoordinates2 = textureCoordinates * texOffsets.zw;
+
+    blend = blendFactor;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0f);
 }
