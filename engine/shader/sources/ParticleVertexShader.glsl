2598d6ccf91a703702fd29b4014a10ba291bb2b4
david <david.marcinkovic@fer.hr>
2021-02-13 17:54:06 +0100

    Fix wrong layout location and position size

--- engine/shader/sources/ParticleVertexShader.glsl
+++ engine/shader/sources/ParticleVertexShader.glsl
@@ -1,9 +1,9 @@
 #version 450 core
 
-layout (location = 0) in vec3 position;
+layout (location = 0) in vec2 position;
 layout (location = 1) in mat4 modelViewMatrix;
-layout (location = 5) in float blendFactor;
-layout (location = 6) in vec4 texOffsets;
+layout (location = 5) in vec4 texOffsets;
+layout (location = 6) in float blendFactor;
 
 out vec2 textureCoordinates1;
 out vec2 textureCoordinates2;
@@ -14,7 +14,7 @@ uniform int numberOfRows;
 
 void main()
 {
-    vec2 textureCoordinates = position.xy + vec2(0.5, 0.5);
+    vec2 textureCoordinates = position + vec2(0.5, 0.5);
 
     textureCoordinates.y = 1.0 - textureCoordinates.y;
     textureCoordinates /= float(numberOfRows);
@@ -23,5 +23,5 @@ void main()
     textureCoordinates2 = textureCoordinates * texOffsets.zw;
 
     blend = blendFactor;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0f);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 0.0, 1.0f);
 }
