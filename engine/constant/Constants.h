93fab4355513ede4b4d7506b790748d1664dc6f0
david <david.marcinkovic@fer.hr>
2021-02-14 19:52:11 +0100

    Define constant for clear color in Constants header

--- engine/constant/Constants.h
+++ engine/constant/Constants.h
@@ -17,6 +17,7 @@ struct Constants
 	constexpr static const int SHADOW_WIDTH = 1024;
 	constexpr static const int SHADOW_HEIGHT = 1024;
 	constexpr static double GRAVITY = -50;
+	constexpr const static glm::vec4 CLEAR_COLOR{0.5f, 0.5f, 0.5f, 1.0f};
 };
 
 #endif //SURVIVE_CONSTANTS_H
