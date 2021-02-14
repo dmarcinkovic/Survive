b27f0d7f088a722d9ee603f4440593f9f4375619
david <david.marcinkovic@fer.hr>
2021-02-14 16:25:00 +0100

    Created ParticleModel class

---
+++ engine/particles/ParticleModel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 14. 02. 2021..
+//
+
+#include "ParticleModel.h"
---
+++ engine/particles/ParticleModel.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 14. 02. 2021..
+//
+
+#ifndef SURVIVE_PARTICLEMODEL_H
+#define SURVIVE_PARTICLEMODEL_H
+
+
+class ParticleModel
+{
+
+};
+
+
+#endif //SURVIVE_PARTICLEMODEL_H
