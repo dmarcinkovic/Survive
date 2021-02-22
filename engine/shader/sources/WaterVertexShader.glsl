2a6f9e93285fbe8c103246285179b2c6b55c1314
david <david.marcinkovic@fer.hr>
2021-02-22 21:02:56 +0100

    Draw simple quad rotated around x axis by 90 degrees

--- engine/shader/sources/WaterVertexShader.glsl
+++ engine/shader/sources/WaterVertexShader.glsl
@@ -1,5 +1,15 @@
-#version 120
+#version 450 core
 
-void main() {
+layout (location = 0) in vec2 position;
 
+out vec2 textureCoordinates;
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 transformationMatrix;
+
+void main()
+{
+    gl_Position = projectionMatrix * viewMatrix * transformationMatrix * vec4(position.x, 0.0, position.y, 1.0);
+    textureCoordinates = vec2(position.x / 2.0 + 0.5, position.y / 2.0 + 0.5);
 }
