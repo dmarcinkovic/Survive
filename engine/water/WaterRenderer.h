340510a5f961cfca608fd8a36212205e91b0dc2b
david <david.marcinkovic@fer.hr>
2021-02-22 20:42:01 +0100

    Created water renderer class

---
+++ engine/water/WaterRenderer.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 22. 02. 2021..
+//
+
+#ifndef SURVIVE_WATERRENDERER_H
+#define SURVIVE_WATERRENDERER_H
+
+
+class WaterRenderer
+{
+
+};
+
+
+#endif //SURVIVE_WATERRENDERER_H
