7225021daacbd201f85a8e94bf5ce5116894043d
david <david.marcinkovic@fer.hr>
2021-02-22 21:27:26 +0100

    Created method for preparing and finishing rendering

--- engine/water/WaterRenderer.cpp
+++ engine/water/WaterRenderer.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "WaterRenderer.h"
+#include "../renderer/Renderer3DUtil.h"
+#include "../math/Maths.h"
+
+WaterRenderer::WaterRenderer()
+{
+
+}
+
+void WaterRenderer::render(const Camera &camera, const Light &light) const
+{
+	prepareRendering(camera);
+
+	for (auto const &waterTile : m_Tiles)
+	{
+		auto const &water = waterTile.get();
+
+
+	}
+
+	finishRendering();
+}
+
+void WaterRenderer::prepareRendering(const Camera &camera) const
+{
+	Renderer3DUtil::prepareRendering(m_Shader);
+	glEnableVertexAttribArray(0);
+	Renderer3DUtil::addTransparency(true, true);
+
+	m_Shader.loadProjectionMatrix(Maths::projectionMatrix);
+	m_Shader.loadViewMatrix(Maths::createViewMatrix(camera));
+}
+
+void WaterRenderer::finishRendering()
+{
+	TexturedModel::unbindVao();
+	glDisableVertexAttribArray(0);
+	Renderer3DUtil::addTransparency(false, false);
+	Renderer3DUtil::finishRendering();
+}
--- engine/water/WaterRenderer.h
+++ engine/water/WaterRenderer.h
@@ -5,10 +5,31 @@
 #ifndef SURVIVE_WATERRENDERER_H
 #define SURVIVE_WATERRENDERER_H
 
+#include <functional>
+
+#include "../texture/Texture.h"
+#include "WaterShader.h"
+#include "WaterTile.h"
+#include "../light/Light.h"
+#include "../camera/Camera.h"
 
 class WaterRenderer
 {
+private:
+	WaterShader m_Shader;
+	std::vector<Texture> m_Textures;
 
+	std::vector<std::reference_wrapper<WaterTile>> m_Tiles;
+
+public:
+	WaterRenderer();
+
+	void render(const Camera &camera, const Light &light) const;
+
+private:
+	void prepareRendering(const Camera &camera) const;
+
+	static void finishRendering();
 };
 
 
