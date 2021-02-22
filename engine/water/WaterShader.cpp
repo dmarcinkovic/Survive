a23d15414e965988d42d6f298ada91b9df75460b
david <david.marcinkovic@fer.hr>
2021-02-22 20:42:30 +0100

    Created WaterShader class

---
+++ engine/water/WaterShader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 22. 02. 2021..
+//
+
+#include "WaterShader.h"
---
+++ engine/water/WaterShader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 22. 02. 2021..
+//
+
+#ifndef SURVIVE_WATERSHADER_H
+#define SURVIVE_WATERSHADER_H
+
+
+class WaterShader
+{
+
+};
+
+
+#endif //SURVIVE_WATERSHADER_H
