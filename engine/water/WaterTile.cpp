c5911841bae28c21a0a59d73de12b3795374347b
david <david.marcinkovic@fer.hr>
2021-02-22 20:47:30 +0100

    Write WaterTile class

--- engine/water/WaterTile.cpp
+++ engine/water/WaterTile.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "WaterTile.h"
+
+WaterTile::WaterTile(const Model &model, float centerX, float height, float centerZ, const Texture &duDvMap,
+					 const Texture &normalMap)
+		: Entity(TexturedModel(model, 0), glm::vec3{centerX, height, centerZ},
+				 glm::vec3{TILE_SIZE, TILE_SIZE, TILE_SIZE}), m_DuDvMap(duDvMap), m_NormalMap(normalMap)
+{
+
+}
+
+const Texture &WaterTile::getNormalMap() const
+{
+	return m_NormalMap;
+}
+
+const Texture &WaterTile::getDuDvMap() const
+{
+	return m_DuDvMap;
+}
--- engine/water/WaterTile.h
+++ engine/water/WaterTile.h
@@ -6,9 +6,22 @@
 #define SURVIVE_WATERTILE_H
 
 
-class WaterTile
-{
+#include "../entity/Entity.h"
 
+class WaterTile : public Entity
+{
+private:
+	static constexpr int TILE_SIZE = 200;
+
+	Texture m_DuDvMap;
+	Texture m_NormalMap;
+
+public:
+	WaterTile(const Model &model, float centerX, float  height, float centerZ, const Texture &duDvMap, const Texture &normalMap);
+
+	[[nodiscard]] const Texture &getNormalMap() const;
+
+	[[nodiscard]] const Texture &getDuDvMap() const;
 };
 
 
