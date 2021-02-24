cf0f50061aada3a7fe239b28dfbc89b83b36db0d
david <david.marcinkovic@fer.hr>
2021-02-24 23:10:51 +0100

    Tile texture coordinates

--- engine/shader/sources/WaterVertexShader.glsl
+++ engine/shader/sources/WaterVertexShader.glsl
@@ -9,9 +9,11 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 transformationMatrix;
 
+const float tilling = 6.0f;
+
 void main()
 {
     clipSpace = projectionMatrix * viewMatrix * transformationMatrix * vec4(position.x, 0.0, position.y, 1.0);
     gl_Position =  clipSpace;
-    textureCoordinates = vec2(position.x / 2.0 + 0.5, position.y / 2.0 + 0.5);
+    textureCoordinates = vec2(position.x / 2.0 + 0.5, position.y / 2.0 + 0.5) * tilling;
 }
