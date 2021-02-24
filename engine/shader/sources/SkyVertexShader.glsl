23aead5eb4c41d063d1bee992def3cfaa013f15f
david <david.marcinkovic@fer.hr>
2021-02-24 21:33:35 +0100

    Define clip distance variable in SkyBox vertex shader

--- engine/shader/sources/SkyVertexShader.glsl
+++ engine/shader/sources/SkyVertexShader.glsl
@@ -7,10 +7,14 @@ out vec3 textureCoordinates;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 transformationMatrix;
+uniform vec4 plane;
 
 void main()
 {
     textureCoordinates = position;
-    vec4 pos = projectionMatrix * viewMatrix * transformationMatrix * vec4(position, 1.0);
-    gl_Position = pos;
+
+    vec4 pos = transformationMatrix * vec4(position, 1.0);
+    gl_ClipDistance[0] = dot(pos, plane);
+
+    gl_Position = projectionMatrix * viewMatrix * pos;
 }
