838445312f8ee2242a947328cb5d37186e00f4df
david <david.marcinkovic@fer.hr>
2021-02-24 23:33:57 +0100

    Implement Fresnel effect in water fragment shader

--- engine/shader/sources/WaterVertexShader.glsl
+++ engine/shader/sources/WaterVertexShader.glsl
@@ -4,6 +4,7 @@ layout (location = 0) in vec2 position;
 
 out vec2 textureCoordinates;
 out vec4 clipSpace;
+out vec3 toCameraVector;
 
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
@@ -14,7 +15,11 @@ const float tilling = 6.0f;
 
 void main()
 {
-    clipSpace = projectionMatrix * viewMatrix * transformationMatrix * vec4(position.x, 0.0, position.y, 1.0);
+    vec4 worldPosition = transformationMatrix * vec4(position.x, 0.0, position.y, 1.0);
+    clipSpace = projectionMatrix * viewMatrix * worldPosition;
     gl_Position =  clipSpace;
+
     textureCoordinates = vec2(position.x / 2.0 + 0.5, position.y / 2.0 + 0.5) * tilling;
+
+    toCameraVector = cameraPosition - worldPosition.xyz;
 }
