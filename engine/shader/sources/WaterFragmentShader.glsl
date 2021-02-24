0d4061cf5a3621140dc658038bb789eb8016254e
david <david.marcinkovic@fer.hr>
2021-02-24 23:12:24 +0100

    Calculate distortion

--- engine/shader/sources/WaterFragmentShader.glsl
+++ engine/shader/sources/WaterFragmentShader.glsl
@@ -16,6 +16,8 @@ void main()
     vec2 refractionTextureCoordinates = normalizedDeviceCoordinates;
     vec2 reflectionTextureCoordinates = vec2(normalizedDeviceCoordinates.x, -normalizedDeviceCoordinates.y);
 
+    vec2 distortion1 = texture(duDvMap, vec2(textureCoordinates.x, textureCoordinates.y)).rg * 2.0f - 1.0f;
+
     vec4 reflectionColor = texture(reflectionTexture, reflectionTextureCoordinates);
     vec4 refractionColor = texture(refractionTexture, refractionTextureCoordinates);
 
