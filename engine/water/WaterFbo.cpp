93316dd27979c96ecb5176875980d4ccfc3d6f17
david <david.marcinkovic@fer.hr>
2021-02-25 22:11:35 +0100

    Use createDepthTextureAttachment function instead of createDepthTexture function

--- engine/water/WaterFbo.cpp
+++ engine/water/WaterFbo.cpp
@@ -16,7 +16,7 @@ void WaterFbo::initializeRefractionFrameBuffer()
 	m_FboRefraction.bindFrameBuffer();
 	FrameBuffer::drawBuffer();
 	m_RefractionColorTexture = Texture(m_FboRefraction.attachColorComponent(REFRACTION_WIDTH, REFRACTION_HEIGHT));
-	m_RefractionDepthTexture = Texture(m_FboRefraction.createDepthTexture(REFRACTION_WIDTH, REFRACTION_HEIGHT));
+	m_RefractionDepthTexture = Texture(m_FboRefraction.createDepthTextureAttachment(REFRACTION_WIDTH, REFRACTION_HEIGHT));
 	m_FboRefraction.attachDepthComponent(REFRACTION_WIDTH, REFRACTION_HEIGHT);
 
 	unbindFrameBuffer();
