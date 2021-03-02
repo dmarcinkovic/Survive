0299177cf598656b180b618ac8815fbf8b55bd24
david <david.marcinkovic@fer.hr>
2021-03-02 23:48:41 +0100

    Created small testing code that uses some entt functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include "engine/display/Display.h"
 #include "engine/renderer/Loader.h"
 #include "engine/renderer/Renderer3D.h"
 #include "engine/parser/ObjParser.h"
+#include "ecs/entt.hpp"
 
 int main()
 {
@@ -15,6 +17,45 @@ int main()
 	Light light(glm::vec3{100, 100, 100}, glm::vec3{1.0f, 1.0f, 1.0f});
 
 	Renderer3D renderer(light);
+
+	struct TransformComponent {
+		glm::vec3 pos;
+	};
+
+	struct RenderComponent {
+		glm::vec3 color;
+	};
+
+	struct IdComponent {
+		std::uint32_t id;
+
+		IdComponent() = default;
+
+		explicit IdComponent(std::uint32_t id)
+			: id(id) {}
+	};
+
+	entt::registry registry;
+
+	auto entity1 = registry.create();
+	registry.emplace<TransformComponent>(entity1);
+	registry.emplace<IdComponent>(entity1, 1);
+
+	auto entity2 = registry.create();
+	registry.emplace<RenderComponent>(entity2);
+	registry.emplace<IdComponent>(entity2, 2);
+
+	auto entity3 = registry.create();
+	registry.emplace<RenderComponent>(entity3);
+	registry.emplace<TransformComponent>(entity3);
+	registry.emplace<IdComponent>(entity3, 3);
+
+	auto group = registry.view<TransformComponent, IdComponent>();
+	for (auto entity : group)
+	{
+		auto idComponent = group.get<IdComponent>(entity);
+		std::cout << "Entity with ID: " << idComponent.id << '\n';
+	}
 
 	auto textures = Loader::loadTextures({"res/lamp_bloom.png", "res/lamp_bloom_emissive.png"});
 
