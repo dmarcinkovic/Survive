ba67bb967f3693bfae24272b733d843e115f0b8b
david <david.marcinkovic@fer.hr>
2021-03-04 00:08:58 +0100

    Render terrain in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "engine/renderer/Loader.h"
 #include "engine/renderer/Renderer3D.h"
 #include "engine/parser/ObjParser.h"
+#include "engine/terrain/TerrainGenerator.h"
 
 int main()
 {
@@ -15,6 +16,11 @@ int main()
 	Light light(glm::vec3{100, 100, 100}, glm::vec3{1.0f, 1.0f, 1.0f});
 
 	Renderer3D renderer(light);
+
+	Terrain terrain(TerrainGenerator::generateTerrain(loader, "res/heightmap.jpeg"), glm::vec3{-200, -10, -200},
+					glm::vec3{400, 1, 400});
+	terrain.addTextures("res/blendMap.png", {"res/dirt.png", "res/grass.jpeg", "res/rock.png", "res/flowers.png"});
+	renderer.addTerrain(terrain);
 
 	auto textures = Loader::loadTextures({"res/lamp_bloom.png", "res/lamp_bloom_emissive.png"});
 
