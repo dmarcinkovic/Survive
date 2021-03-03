fd15536d8a7886c963cee2c787d352a00cc649a9
david <david.marcinkovic@fer.hr>
2021-03-03 21:18:57 +0100

    Render sprites in separate method and remove addSprite method

--- engine/sprites/SpritesRenderer.cpp
+++ engine/sprites/SpritesRenderer.cpp
@@ -6,43 +6,28 @@
 #include "../math/Maths.h"
 #include "../renderer/Renderer2DUtil.h"
 #include "../components/RenderComponent.h"
-#include "../components/TransformComponent.h"
 
-void SpritesRenderer::renderSprite(entt::registry &registry) const
+
+void SpritesRenderer::render(entt::registry &registry) const
 {
-	if (m_Sprites.empty())
+	auto entities = prepareEntities(registry);
+
+	if (entities.empty())
 	{
 		return;
 	}
 
 	Renderer2DUtil::prepareRendering(m_Shader);
 
-	for (auto const&[texture, entities] : m_Sprites)
+	for (auto const&[texture, sprites] : entities)
 	{
 		Renderer2DUtil::prepareEntity(texture);
-		for (auto const &sprite : entities)
-		{
-			TransformComponent transformComponent = registry.get<TransformComponent>(sprite);
-			Sprite &spriteComponent = registry.get<Sprite>(sprite);
-
-			loadUniforms(transformComponent, spriteComponent);
-			animate(spriteComponent);
-
-			glDrawElements(GL_TRIANGLES, texture.vertexCount(), GL_UNSIGNED_INT, nullptr);
-		}
+		renderSprites(sprites, registry, texture);
 
 		Renderer2DUtil::finishRenderingEntity();
 	}
 
 	Renderer2DUtil::finishRendering();
-}
-
-void SpritesRenderer::addSprite(const entt::registry &registry, entt::entity entity) noexcept
-{
-	RenderComponent renderComponent = registry.get<RenderComponent>(entity);
-
-	std::vector<entt::entity> &batch = m_Sprites[renderComponent.texturedModel];
-	batch.emplace_back(entity);
 }
 
 void SpritesRenderer::animate(Sprite &sprite)
@@ -57,4 +42,36 @@ void SpritesRenderer::loadUniforms(const TransformComponent &transform, const Sp
 	m_Shader.loadTransformationMatrix(modelMatrix);
 	m_Shader.loadSpriteSize(sprite.row, sprite.col);
 	m_Shader.loadSpritePosition(sprite.getFrameIndex());
+}
+
+std::unordered_map<TexturedModel, std::vector<entt::entity>, TextureHash>
+SpritesRenderer::prepareEntities(entt::registry &registry)
+{
+	auto view = registry.view<RenderComponent, TransformComponent, Sprite>();
+
+	std::unordered_map<TexturedModel, std::vector<entt::entity>, TextureHash> entities;
+	for (auto const &entity : view)
+	{
+		RenderComponent renderComponent = view.get<RenderComponent>(entity);
+
+		std::vector<entt::entity> &batch = entities[renderComponent.texturedModel];
+		batch.emplace_back(entity);
+	}
+
+	return entities;
+}
+
+void SpritesRenderer::renderSprites(const std::vector<entt::entity> &sprites, entt::registry &registry,
+									const TexturedModel &texturedModel) const
+{
+	for (auto const &sprite : sprites)
+	{
+		TransformComponent transformComponent = registry.get<TransformComponent>(sprite);
+		Sprite &spriteComponent = registry.get<Sprite>(sprite);
+
+		loadUniforms(transformComponent, spriteComponent);
+		animate(spriteComponent);
+
+		glDrawElements(GL_TRIANGLES, texturedModel.vertexCount(), GL_UNSIGNED_INT, nullptr);
+	}
 }
