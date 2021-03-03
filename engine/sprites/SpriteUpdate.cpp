0112008b6bf2f2d964a36fa46c84ec1a06340451
david <david.marcinkovic@fer.hr>
2021-03-03 21:48:02 +0100

    Created SpriteUpdate system that will update sprites every frame

---
+++ engine/sprites/SpriteUpdate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 03. 03. 2021..
+//
+
+#include "SpriteUpdate.h"
---
+++ engine/sprites/SpriteUpdate.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 03. 03. 2021..
+//
+
+#ifndef SURVIVE_SPRITEUPDATE_H
+#define SURVIVE_SPRITEUPDATE_H
+
+
+class SpriteUpdate
+{
+
+};
+
+
+#endif //SURVIVE_SPRITEUPDATE_H
