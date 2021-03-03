e2f3f566a843b5715e428a86aff659a5eda3265b
david <david.marcinkovic@fer.hr>
2021-03-03 23:31:25 +0100

    Render simple text

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "engine/display/Display.h"
 #include "engine/renderer/Loader.h"
 #include "ecs/entt.hpp"
-#include "engine/components/TransformComponent.h"
-#include "engine/components/RenderComponent.h"
-#include "engine/sprites/SpritesRenderer.h"
-#include "engine/sprites/SpriteUpdate.h"
+#include "engine/text/Text.h"
+#include "engine/text/TextRenderer.h"
 
 int main()
 {
@@ -14,30 +12,23 @@ int main()
 	Display display(width, height, "Survive");
 	Loader loader;
 
-	SpritesRenderer spritesRenderer;
-	GuiRenderer guiRenderer;
+	Font font("res/arial.png", loader);
+	font.loadFontFromFntFile("res/arial.fnt");
+	Text text("Some text", font, glm::vec3{}, glm::vec3{1,0,0}, 1);
+	text.centerText();
+
+	TextRenderer textRenderer;
+	textRenderer.addText(text, loader);
 
 	entt::registry registry;
 
 	auto entity = registry.create();
 
-	registry.emplace<RenderComponent>(entity,
-									  TexturedModel(loader.renderQuad(), Loader::loadTexture("res/boy.png")));
-	registry.emplace<TransformComponent>(entity, glm::vec3{0.5, 0.5, 0}, glm::vec3{0.5, 0.5, 0});
-	registry.emplace<SpriteSheetComponent>(entity, 2, 4, 6, 0, 0, 0, 3);
-
-	auto gui = registry.create();
-	registry.emplace<RenderComponent>(gui, TexturedModel(loader.renderQuad(), Loader::loadTexture("res/circle.png")));
-	registry.emplace<TransformComponent>(gui, glm::vec3{-0.5, 0.5, 0}, glm::vec3{0.5, 0.5, 0});
-
 	while (display.isRunning())
 	{
 		Display::clearWindow();
 
-		spritesRenderer.render(registry);
-		guiRenderer.render(registry);
-
-		SpriteUpdate::update(registry);
+		textRenderer.renderText();
 
 		display.update();
 	}
