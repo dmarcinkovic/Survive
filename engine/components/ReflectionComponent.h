c5c2b03d785995ca93c3e1f689e8ca5a0a68647f
david <david.marcinkovic@fer.hr>
2021-03-05 22:22:34 +0100

    Created ReflectionComponent

---
+++ engine/components/ReflectionComponent.h
@@ -0,0 +1,13 @@
+//
+// Created by david on 05. 03. 2021..
+//
+
+#ifndef SURVIVE_REFLECTIONCOMPONENT_H
+#define SURVIVE_REFLECTIONCOMPONENT_H
+
+struct ReflectionComponent
+{
+
+};
+
+#endif //SURVIVE_REFLECTIONCOMPONENT_H
