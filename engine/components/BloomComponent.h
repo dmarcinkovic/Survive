4e49d584bb9e4bd9e74cb4da2e0bb68b31226961
david <david.marcinkovic@fer.hr>
2021-03-05 22:08:26 +0100

    Created BloomComponent

---
+++ engine/components/BloomComponent.h
@@ -0,0 +1,13 @@
+//
+// Created by david on 05. 03. 2021..
+//
+
+#ifndef SURVIVE_BLOOMCOMPONENT_H
+#define SURVIVE_BLOOMCOMPONENT_H
+
+struct BloomComponent
+{
+	
+};
+
+#endif //SURVIVE_BLOOMCOMPONENT_H
