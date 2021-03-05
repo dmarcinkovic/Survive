736fe7714de92b00af572571eeb2894bfae66b0e
david <david.marcinkovic@fer.hr>
2021-03-05 22:24:57 +0100

    Add required data into ReflectionComponent

--- engine/components/ReflectionComponent.h
+++ engine/components/ReflectionComponent.h
@@ -5,9 +5,18 @@
 #ifndef SURVIVE_REFLECTIONCOMPONENT_H
 #define SURVIVE_REFLECTIONCOMPONENT_H
 
+#include "../texture/Texture.h"
+
 struct ReflectionComponent
 {
+	Texture reflectionTexture{};
+	float reflectionFactor{};
 
+	ReflectionComponent(const Texture &reflectionTexture, float reflectionFactor)
+		: reflectionTexture(reflectionTexture), reflectionFactor(reflectionFactor)
+	{}
+
+	ReflectionComponent() = default;
 };
 
 #endif //SURVIVE_REFLECTIONCOMPONENT_H
