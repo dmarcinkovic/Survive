2441c5f8e25ce75e3c365ba75dfcd9cdb3c00131
david <david.marcinkovic@fer.hr>
2021-03-05 23:35:29 +0100

    Created Components.h header that includes all other Components

---
+++ engine/components/Components.h
@@ -0,0 +1,19 @@
+//
+// Created by david on 05. 03. 2021..
+//
+
+#ifndef SURVIVE_COMPONENTS_H
+#define SURVIVE_COMPONENTS_H
+
+#include "BloomComponent.h"
+#include "IdComponent.h"
+#include "ReflectionComponent.h"
+#include "RefractionComponent.h"
+#include "RenderComponent.h"
+#include "RigidBodyComponent.h"
+#include "SpriteComponent.h"
+#include "SpriteSheetComponent.h"
+#include "Transform2DComponent.h"
+#include "Transform3DComponent.h"
+
+#endif //SURVIVE_COMPONENTS_H
