11ee8c61b940bb6412098860b999da41c3f0eb8c
david <david.marcinkovic@fer.hr>
2021-03-05 22:47:28 +0100

    Created Transform2DComponent

---
+++ engine/components/Transform2DComponent.h
@@ -0,0 +1,24 @@
+//
+// Created by david on 05. 03. 2021..
+//
+
+#ifndef SURVIVE_TRANSFORM2DCOMPONENT_H
+#define SURVIVE_TRANSFORM2DCOMPONENT_H
+
+#include <glm/glm.hpp>
+
+struct Transform3DComponent
+{
+	glm::vec2 position;
+	glm::vec2 rotation;
+	glm::vec2 scale;
+
+	Transform3DComponent() = default;
+
+	explicit Transform3DComponent(const glm::vec2 &position, const glm::vec2 &scale = glm::vec2{1.0f},
+								  const glm::vec2 &rotation = glm::vec2{0.0f})
+			: position(position), rotation(rotation), scale(scale)
+	{}
+};
+
+#endif //SURVIVE_TRANSFORM2DCOMPONENT_H
