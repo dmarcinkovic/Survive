528ed8737c6385f101a05f52823e6744c376fd60
david <david.marcinkovic@fer.hr>
2021-03-06 13:18:43 +0100

    Render object that casts shadow to terrain

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@
 #include "engine/terrain/Terrain.h"
 #include "engine/terrain/TerrainGenerator.h"
 #include "engine/terrain/TerrainRenderer.h"
+#include "engine/fbo/FrameBuffer.h"
+#include "engine/shadowMapping/ShadowRenderer.h"
+#include "engine/constant/Constants.h"
+#include "engine/parser/ObjParser.h"
+#include "engine/objects/ObjectRenderer.h"
 
 int main()
 {
@@ -16,26 +21,42 @@ int main()
 	Loader loader;
 
 	Light light(glm::vec3{100, 100, 100}, glm::vec3{1, 1, 1});
-
 	Camera camera;
+	ObjectRenderer objectRenderer(light);
 
 	entt::registry registry;
 
-	auto entity = registry.create();
+	auto terrain = registry.create();
 
 	TerrainRenderer terrainRenderer;
 
-	registry.emplace<Transform3DComponent>(entity, glm::vec3{-200, -10, -200}, glm::vec3{400, 1, 400});
-	registry.emplace<RenderComponent>(entity,
+	registry.emplace<Transform3DComponent>(terrain, glm::vec3{-200, -10, -200}, glm::vec3{400, 1, 400});
+	registry.emplace<RenderComponent>(terrain,
 									  TexturedModel(TerrainGenerator::generateTerrain(loader, "res/heightmap.jpeg"),
 													Loader::loadTexture("res/blendMap.png")));
-	registry.emplace<TexturedComponent>(entity, Loader::loadAllTextures({"res/dirt.png", "res/grass.jpeg", "res/rock.png", "res/flowers.png"}));
+	registry.emplace<TexturedComponent>(terrain, Loader::loadAllTextures(
+			{"res/dirt.png", "res/grass.jpeg", "res/rock.png", "res/flowers.png"}));
+
+	auto dragon = registry.create();
+	registry.emplace<RenderComponent>(dragon, TexturedModel(ObjParser::loadObj("res/dragon.obj", loader),
+															Loader::loadTexture("res/lamp.jpg")));
+	registry.emplace<Transform3DComponent>(dragon, glm::vec3{0, 0, -30});
+	registry.emplace<RigidBodyComponent>(dragon, false);
+
+	FrameBuffer frameBuffer;
+	GLuint shadowMap = frameBuffer.attachToDepthBufferTexture(Constants::SHADOW_WIDTH, Constants::SHADOW_HEIGHT);
+
+	ShadowRenderer shadowRenderer;
+	shadowRenderer.add3DObject(registry, dragon);
 
 	while (display.isRunning())
 	{
 		Display::clearWindow();
 
-		terrainRenderer.render(registry, camera, light, 0);
+		frameBuffer.renderToFrameBuffer(registry, shadowRenderer, camera, light, Constants::SHADOW_WIDTH,
+										Constants::SHADOW_HEIGHT);
+		terrainRenderer.render(registry, camera, light, shadowMap);
+		objectRenderer.render(registry, camera, shadowMap);
 
 		display.update();
 	}
