128b687877818b654e166931d856f27e0e3e2920
david <david.marcinkovic@fer.hr>
2021-03-09 00:21:05 +0100

    Render sky

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 #include "ecs/entt.hpp"
 #include "engine/light/Light.h"
 #include "engine/camera/Camera.h"
-#include "engine/terrain/TerrainGenerator.h"
-#include "engine/parser/ObjParser.h"
-#include "engine/renderer/Renderer3D.h"
+#include "engine/entity/Entity.h"
+#include "engine/sky/SkyRenderer.h"
 
 int main()
 {
@@ -15,44 +14,23 @@ int main()
 	Display display(width, height, "Survive");
 	Loader loader;
 
+	TexturedModel texturedModel(loader.renderCube(), Loader::loadCubeMap(
+			{"res/right.png", "res/left.png", "res/top.png", "res/bottom.png", "res/front.png", "res/back.png"}));
+	Entity sky(texturedModel, glm::vec3{}, glm::vec3{500});
+
 	Light light(glm::vec3{100, 100, 100}, glm::vec3{1, 1, 1});
 	Camera camera;
 
-	Renderer3D renderer(light);
-
 	entt::registry registry;
 
-	auto terrain = registry.create();
-
-	registry.emplace<Transform3DComponent>(terrain, glm::vec3{-200, -10, -200}, glm::vec3{400, 1, 400});
-	registry.emplace<RenderComponent>(terrain,
-									  TexturedModel(TerrainGenerator::generateTerrain(loader, "res/heightmap.jpeg"),
-													Loader::loadTexture("res/blendMap.png")));
-	registry.emplace<TexturedComponent>(terrain, Loader::loadAllTextures(
-			{"res/dirt.png", "res/grass.jpeg", "res/rock.png", "res/flowers.png"}));
-
-	auto dragon = registry.create();
-	registry.emplace<RenderComponent>(dragon, TexturedModel(ObjParser::loadObj("res/dragon.obj", loader),
-															Loader::loadTexture("res/lamp.jpg")));
-	registry.emplace<Transform3DComponent>(dragon, glm::vec3{-5, 0, -30});
-	registry.emplace<RigidBodyComponent>(dragon, false);
-
-	renderer.addShadow(registry, dragon);
-
-	auto dragon2 = registry.create();
-	registry.emplace<RenderComponent>(dragon2, TexturedModel(ObjParser::loadObj("res/dragon.obj", loader),
-															Loader::loadTexture("res/lamp.jpg")));
-	registry.emplace<Transform3DComponent>(dragon2, glm::vec3{5, 0, -30});
-	registry.emplace<RigidBodyComponent>(dragon2, false);
-
-	renderer.addShadow(registry, dragon2);
-	renderer.addOutlineToObject(registry, dragon2);
+	SkyRenderer renderer;
+	renderer.addSkyEntity(sky);
 
 	while (display.isRunning())
 	{
 		Display::clearWindow();
 
-		renderer.render(registry, camera);
+		renderer.render(camera);
 
 		display.update();
 	}
