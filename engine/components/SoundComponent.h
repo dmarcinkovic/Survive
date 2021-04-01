4a87247037962c7e3ce5c72f8bcee2362ff6a71b
david <david.marcinkovic@fer.hr>
2021-04-02 01:01:01 +0200

    Created AudioComponent

--- engine/components/SoundComponent.h
+++ engine/components/SoundComponent.h
@@ -5,4 +5,17 @@
 #ifndef SURVIVE_SOUNDCOMPONENT_H
 #define SURVIVE_SOUNDCOMPONENT_H
 
+#include "../audio/Source.h"
+
+struct SoundComponent
+{
+	Source audioSource;
+
+	SoundComponent() = default;
+
+	SoundComponent(Source source)
+			: audioSource(std::move(source))
+	{}
+};
+
 #endif //SURVIVE_SOUNDCOMPONENT_H
