09308b09dca6adb714a4b271906951e1b20714e9
david <david.marcinkovic@fer.hr>
2021-04-09 20:22:04 +0200

    Set different default log window size

--- editor/editor/Log.h
+++ editor/editor/Log.h
@@ -23,7 +23,7 @@ private:
 
 public:
 	static void
-	logWindow(const char *message, const ImVec2 &size = ImVec2(300, 100), double time = 3.0);
+	logWindow(const char *message, const ImVec2 &size = ImVec2(300, 50), double time = 3.0);
 
 private:
 	static void drawLogWindow();
