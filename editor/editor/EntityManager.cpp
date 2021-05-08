4d2c2278521136d33e101a3d5a10c0b7f340c0d8
david <david.marcinkovic@fer.hr>
2021-05-08 13:04:44 +0200

    Change input text backgroud color

--- editor/editor/EntityManager.cpp
+++ editor/editor/EntityManager.cpp
@@ -30,7 +30,7 @@ void Survive::EntityManager::addEntity(entt::registry &registry)
 
 void Survive::EntityManager::createEntity(entt::registry &registry)
 {
-	ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 0.2, 0.2, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(0.1f, 0.2f, 0.3f, 1.0f));
 	if (ImGui::InputText("Entity name", buffer, BUFFER_SIZE,
 						 ImGuiInputTextFlags_CtrlEnterForNewLine | ImGuiInputTextFlags_EnterReturnsTrue))
 	{
@@ -46,11 +46,11 @@ void Survive::EntityManager::createEntity(entt::registry &registry)
 
 void Survive::EntityManager::listEntities(entt::registry &registry)
 {
-	ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.0f,0.2f,0.2f,1.0f));
-	ImGui::PushStyleColor(ImGuiCol_HeaderActive, ImVec4(0.35f,0.5f,0.5f,1.0f));
+	ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.0f, 0.2f, 0.2f, 1.0f));
+	ImGui::PushStyleColor(ImGuiCol_HeaderActive, ImVec4(0.35f, 0.5f, 0.5f, 1.0f));
 	ImGui::PushStyleColor(ImGuiCol_HeaderHovered, ImVec4(0.1f, 0.3f, 0.3f, 1.0f));
 
-	registry.each([&](const entt::entity entity){
+	registry.each([&](const entt::entity entity) {
 		const TagComponent &tag = registry.get<TagComponent>(entity);
 
 		ImGui::CollapsingHeader(tag.tag.c_str());
