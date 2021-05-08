b79d0e35a2146a8e1690a1a38b4ccc3ac8fcd9d6
david <david.marcinkovic@fer.hr>
2021-05-08 11:49:14 +0200

    Created EntityManager class

---
+++ editor/editor/EntityManager.cpp
@@ -0,0 +1,34 @@
+//
+// Created by david on 08. 05. 2021..
+//
+
+#include <imgui.h>
+
+#include "EntityManager.h"
+
+void Survive::EntityManager::addEntity(entt::registry &registry)
+{
+	ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.4f, 0.4f, 0.4f, 1.0f));
+	if (ImGui::Button("Create new entity"))
+	{
+		ImGui::OpenPopup("Create entity");
+	}
+
+	if (ImGui::BeginPopup("Create entity"))
+	{
+		ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 0.2, 0.2, 1.0f));
+		if (ImGui::InputText("Entity name", buffer, BUFFER_SIZE,
+							 ImGuiInputTextFlags_CtrlEnterForNewLine | ImGuiInputTextFlags_EnterReturnsTrue))
+		{
+			ImGui::CloseCurrentPopup();
+		}
+
+		ImGui::PopStyleColor();
+		ImGui::EndPopup();
+	}
+
+	ImGui::Separator();
+
+	ImGui::End();
+	ImGui::PopStyleColor();
+}
---
+++ editor/editor/EntityManager.h
@@ -0,0 +1,24 @@
+//
+// Created by david on 08. 05. 2021..
+//
+
+#ifndef SURVIVE_ENTITYMANAGER_H
+#define SURVIVE_ENTITYMANAGER_H
+
+#include "entt.hpp"
+
+namespace Survive
+{
+	class EntityManager
+	{
+	private:
+		static constexpr int BUFFER_SIZE = 256;
+
+		char buffer[BUFFER_SIZE]{};
+
+	public:
+		void addEntity(entt::registry &registry);
+	};
+}
+
+#endif //SURVIVE_ENTITYMANAGER_H
