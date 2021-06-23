085012e20c22790980e26c3ba84d0a778df5151d
david <david.marcinkovic@fer.hr>
2021-06-23 21:45:42 +0200

    Create SceneLoader class

---
+++ engine/scene/sceneLoader/SceneLoader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 23. 06. 2021..
+//
+
+#include "SceneLoader.h"
---
+++ engine/scene/sceneLoader/SceneLoader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 23. 06. 2021..
+//
+
+#ifndef SURVIVE_SCENELOADER_H
+#define SURVIVE_SCENELOADER_H
+
+
+class SceneLoader
+{
+
+};
+
+
+#endif //SURVIVE_SCENELOADER_H
