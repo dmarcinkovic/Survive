46789e7fc8d602d61414ef62f4ee47368c67f410
david <david.marcinkovic@fer.hr>
2021-07-03 21:14:13 +0200

    Created Component Loader header

---
+++ engine/scene/sceneLoader/ComponentLoader.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 03. 07. 2021..
+//
+
+#ifndef SURVIVE_COMPONENTLOADER_H
+#define SURVIVE_COMPONENTLOADER_H
+
+class ComponentLoader
+{
+public:
+
+};
+
+
+#endif //SURVIVE_COMPONENTLOADER_H
