0adc54a9cacb7d55bee10d71c028dd15ca5aa321
david <david.marcinkovic@fer.hr>
2021-07-03 23:45:00 +0200

    Created loader for Transform3DComponent

--- engine/scene/sceneLoader/ComponentLoader.cpp
+++ engine/scene/sceneLoader/ComponentLoader.cpp
@@ -2,7 +2,6 @@
 // Created by david on 03. 07. 2021..
 //
 
-#include <iostream>
 #include "ObjParser.h"
 #include "ComponentLoader.h"
 #include "Components.h"
@@ -33,13 +32,8 @@ void Survive::ComponentLoader::loadRender2DComponent(entt::registry &registry,
 void Survive::ComponentLoader::loadRender3DComponent(entt::registry &registry, entt::entity entity,
 													 std::ifstream &reader, Loader &loader)
 {
-	std::string modelName, textureName;
-
-	std::getline(reader, modelName);
-	std::getline(reader, textureName);
-
-	modelName = modelName.substr(modelName.find(':') + 1);
-	textureName = textureName.substr(textureName.find(':') + 1);
+	std::string modelName = getName(reader);
+	std::string textureName = getName(reader);
 
 	Render3DComponent render3DComponent(
 			TexturedModel(ObjParser::loadObj(modelName.c_str(), loader),
@@ -54,10 +48,7 @@ void Survive::ComponentLoader::loadRender3DComponent(entt::registry &registry, e
 void Survive::ComponentLoader::loadRigidBodyComponent(entt::registry &registry,
 													  entt::entity entity, std::ifstream &reader)
 {
-	std::string isTransparent;
-
-	std::getline(reader, isTransparent);
-	isTransparent = isTransparent.substr(isTransparent.find(':') + 1);
+	std::string isTransparent = getName(reader);
 
 	registry.emplace<RigidBodyComponent>(entity, std::stoi(isTransparent));
 }
@@ -71,5 +62,32 @@ void Survive::ComponentLoader::loadShadowComponent(entt::registry &registry,
 void Survive::ComponentLoader::loadTransformComponent(entt::registry &registry,
 													  entt::entity entity, std::ifstream &reader)
 {
+	std::string position = getName(reader);
+	std::string scale = getName(reader);
+	std::string rotation = getName(reader);
 
+	parseVec3(position);
+
+	registry.emplace<Transform3DComponent>(entity, parseVec3(position), parseVec3(scale), parseVec3(rotation));
+}
+
+std::string Survive::ComponentLoader::getName(std::ifstream &reader)
+{
+	std::string line;
+	std::getline(reader, line);
+
+	std::string name = line.substr(line.find(':') + 1);
+	return name;
+}
+
+glm::vec3 Survive::ComponentLoader::parseVec3(const std::string &vec3)
+{
+	size_t start = vec3.find(',');
+	size_t end = vec3.find_last_of(',');
+
+	float x = std::stof(vec3.substr(0, start));
+	float y = std::stof(vec3.substr(start + 1, end - start - 1));
+	float z = std::stof(vec3.substr(end + 1));
+
+	return glm::vec3(x, y, z);
 }
--- engine/scene/sceneLoader/ComponentLoader.h
+++ engine/scene/sceneLoader/ComponentLoader.h
@@ -14,22 +14,27 @@ namespace Survive
 	class ComponentLoader
 	{
 	public:
-		static void loadBloomComponent(entt::registry &registry,entt::entity entity, std::ifstream &reader);
+		static void loadBloomComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
-		static void loadReflectionComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader);
+		static void loadReflectionComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
 		static void loadRefractionComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
-		static void loadRender2DComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader);
+		static void loadRender2DComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
-		static void loadRender3DComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader,
+		static void loadRender3DComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader,
 										  Loader &loader);
 
-		static void loadRigidBodyComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader);
+		static void loadRigidBodyComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
-		static void loadShadowComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader);
+		static void loadShadowComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
 
-		static void loadTransformComponent(entt::registry &registry, entt::entity entity,std::ifstream &reader);
+		static void loadTransformComponent(entt::registry &registry, entt::entity entity, std::ifstream &reader);
+
+	private:
+		static std::string getName(std::ifstream &reader);
+
+		static glm::vec3 parseVec3(const std::string &vec3);
 	};
 }
 
