e1925e1f89d5f30f21f3d5a8f9f8106356eaf93b
david <david.marcinkovic@fer.hr>
2021-07-03 17:55:29 +0200

    [WIP] Scene serialization

--- engine/scene/sceneLoader/SceneLoader.cpp
+++ engine/scene/sceneLoader/SceneLoader.cpp
@@ -2,4 +2,30 @@
 // Created by david on 23. 06. 2021..
 //
 
+#include <fstream>
+
 #include "SceneLoader.h"
+
+//entity_name:
+//	Transform3DComponent:
+//		position: 0.5f, 0.123f, 0.1f
+//		scale: 13513
+//		rotation: 124
+//	Render3DComponent:
+//		modelName: "res/nesto"
+//		textureName: "res/opet_nesto"
+//	RigidBody:
+//		isTransparent: false
+
+
+void SceneLoader::loadScene(entt::registry &registry, const std::string &filename)
+{
+	std::ifstream reader(filename);
+
+	std::string line;
+	std::string currentComponent;
+	while(std::getline(reader, line))
+	{
+
+	}
+}
--- engine/scene/sceneLoader/SceneLoader.h
+++ engine/scene/sceneLoader/SceneLoader.h
@@ -5,10 +5,16 @@
 #ifndef SURVIVE_SCENELOADER_H
 #define SURVIVE_SCENELOADER_H
 
+#include "entt.hpp"
+#include "Components.h"
 
 class SceneLoader
 {
+public:
+	static void loadScene(entt::registry &registry, const std::string &filename);
 
+private:
+//	static void loadEntity
 };
 
 
