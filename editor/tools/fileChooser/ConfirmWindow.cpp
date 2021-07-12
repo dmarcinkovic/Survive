676c3a9e504cedae8dbd5914d7fc7f926c9ef477
david <david.marcinkovic@fer.hr>
2021-07-12 21:11:00 +0200

    Create ConfirmWindow class

---
+++ editor/tools/fileChooser/ConfirmWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 12. 07. 2021..
+//
+
+#include "ConfirmWindow.h"
---
+++ editor/tools/fileChooser/ConfirmWindow.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 12. 07. 2021..
+//
+
+#ifndef SURVIVE_CONFIRMWINDOW_H
+#define SURVIVE_CONFIRMWINDOW_H
+
+
+class ConfirmWindow
+{
+
+};
+
+
+#endif //SURVIVE_CONFIRMWINDOW_H
