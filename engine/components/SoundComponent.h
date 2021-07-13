e797b26d552887a793ca44bb1d7d752289fcaac3
david <david.marcinkovic@fer.hr>
2021-07-13 22:39:56 +0200

    Add gain, pitch info in Sound component

--- engine/components/SoundComponent.h
+++ engine/components/SoundComponent.h
@@ -5,18 +5,24 @@
 #ifndef SURVIVE_SOUNDCOMPONENT_H
 #define SURVIVE_SOUNDCOMPONENT_H
 
+#include <AL/al.h>
+
 #include "Source.h"
 
 namespace Survive
 {
 	struct SoundComponent
 	{
+		ALint sound{};
+		float pitch = 1.0f, gain = 1.0f;
+		bool playOnLoop{};
+
 		Source audioSource;
 
 		SoundComponent() = default;
 
-		explicit SoundComponent(const Source &source)
-				: audioSource(source)
+		SoundComponent(ALint sound, const Source &source)
+				: sound(sound), audioSource(source)
 		{}
 	};
 }
