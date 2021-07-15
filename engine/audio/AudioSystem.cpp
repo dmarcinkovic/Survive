6e8aafbd443a16c7b6172b8668db5cd7626d6264
david <david.marcinkovic@fer.hr>
2021-07-15 22:22:01 +0200

    Create AudioSystem class

---
+++ engine/audio/AudioSystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 15. 07. 2021..
+//
+
+#include "AudioSystem.h"
---
+++ engine/audio/AudioSystem.h
@@ -0,0 +1,17 @@
+//
+// Created by david on 15. 07. 2021..
+//
+
+#ifndef SURVIVE_AUDIOSYSTEM_H
+#define SURVIVE_AUDIOSYSTEM_H
+
+namespace Survive
+{
+	class AudioSystem
+	{
+
+	};
+}
+
+
+#endif //SURVIVE_AUDIOSYSTEM_H
