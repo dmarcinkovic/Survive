3f08bacb77fee97df174fccd5356022988f361d8
david <david.marcinkovic@fer.hr>
2021-07-20 22:36:33 +0200

    Remove components that are used for internal purpose

--- editor/tools/components/ComponentUtil.h
+++ editor/tools/components/ComponentUtil.h
@@ -22,11 +22,10 @@ namespace Survive
 		static std::vector<const char *> getListOfComponents()
 		{
 			std::vector<const char *> components = {
-					"Animation component", "Bloom component", "Move component",
-					"Reflection component", "Refraction component", "Render2D Component",
-					"Render3D component", "Rigid body component", "Shadow component",
-					"Sound component", "Sprite component", "Sprite sheet component",
-					"Textured component", "Transform3D component"
+					"Animation component", "Bloom component", "Reflection component",
+					"Refraction component", "Render2D Component", "Render3D component",
+					"Rigid body component", "Shadow component", "Sound component",
+					"Sprite component", "Sprite sheet component", "Transform3D component"
 			};
 
 			return components;
@@ -36,7 +35,6 @@ namespace Survive
 		{
 			drawComponent<AnimationComponent>(registry, entity);
 			drawComponent<BloomComponent>(registry, entity);
-			drawComponent<MoveComponent>(registry, entity);
 			drawComponent<ReflectionComponent>(registry, entity);
 			drawComponent<RefractionComponent>(registry, entity);
 			drawComponent<Render2DComponent>(registry, entity);
@@ -46,7 +44,6 @@ namespace Survive
 			drawComponent<SoundComponent>(registry, entity);
 			drawComponent<SpriteComponent>(registry, entity);
 			drawComponent<SpriteSheetComponent>(registry, entity);
-			drawComponent<TexturedComponent>(registry, entity);
 			drawComponent<Transform3DComponent>(registry, entity);
 		}
 
@@ -55,32 +52,28 @@ namespace Survive
 			switch (selectedItem)
 			{
 				case 0:
-					return addComponent<AnimationComponent>(registry, entity);
+					return addComponent< AnimationComponent>(registry, entity);
 				case 1:
-					return addComponent<BloomComponent>(registry, entity);
+					return addComponent< BloomComponent>(registry, entity);
 				case 2:
-					return addComponent<MoveComponent>(registry, entity);
+					return addComponent< ReflectionComponent>(registry, entity);
 				case 3:
-					return addComponent<ReflectionComponent>(registry, entity);
+					return addComponent< RefractionComponent>(registry, entity);
 				case 4:
-					return addComponent<RefractionComponent>(registry, entity);
+					return addComponent< Render2DComponent>(registry, entity);
 				case 5:
-					return addComponent<Render2DComponent>(registry, entity);
+					return addComponent< Render3DComponent>(registry, entity);
 				case 6:
-					return addComponent<Render3DComponent>(registry, entity);
+					return addComponent< RigidBodyComponent>(registry, entity);
 				case 7:
-					return addComponent<RigidBodyComponent>(registry, entity);
+					return addComponent< ShadowComponent>(registry, entity);
 				case 8:
-					return addComponent<ShadowComponent>(registry, entity);
+					return addComponent< SoundComponent>(registry, entity);
 				case 9:
-					return addComponent<SoundComponent>(registry, entity);
-				case 10:
 					return addComponent<SpriteComponent>(registry, entity);
-				case 11:
+				case 10:
 					return addComponent<SpriteSheetComponent>(registry, entity);
-				case 12:
-					return addComponent<TexturedComponent>(registry, entity);
-				case 13:
+				case 11:
 					return addComponent<Transform3DComponent>(registry, entity);
 				default:
 					throw std::runtime_error("Selected item not implemented");
