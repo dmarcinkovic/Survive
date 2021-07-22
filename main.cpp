2cc0a7a58278885136a802a87c234c9445acce7f
david <david.marcinkovic@fer.hr>
2021-07-22 23:08:55 +0200

    Add terrain to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "AudioSystem.h"
 #include "ObjParser.h"
 #include "EventHandler.h"
 #include "Editor.h"
@@ -7,6 +6,7 @@
 #include "Light.h"
 #include "Renderer.h"
 #include "Display.h"
+#include "TerrainGenerator.h"
 #include "entt.hpp"
 
 int main()
@@ -29,10 +29,26 @@ int main()
 
 	auto dragon = registry.create();
 	registry.emplace<TagComponent>(dragon, "dragon");
-	registry.emplace<Render3DComponent>(dragon, TexturedModel(Model(ObjParser::loadObj("res/dragon.obj", loader)),
-															  Loader::loadTexture("res/lamp.jpg")));
+
+	Render3DComponent renderComponent(TexturedModel(Model(ObjParser::loadObj(
+	"res/dragon.obj", loader)), Loader::loadTexture("res/lamp.jpg")));
+
+	renderComponent.textureName = "res/lamp.jpg";
+	renderComponent.modelName = "res/dragon.obj";
+	registry.emplace<Render3DComponent>(dragon, renderComponent);
 	registry.emplace<RigidBodyComponent>(dragon, false);
-	registry.emplace<Transform3DComponent>(dragon, glm::vec3{0, 0, -20});
+	registry.emplace<Transform3DComponent>(dragon, glm::vec3{0, -5, -40});
+	registry.emplace<ShadowComponent>(dragon, true);
+
+	auto terrain = registry.create();
+	registry.emplace<TagComponent>(terrain, "terrain");
+	registry.emplace<Render3DComponent>(terrain,
+									  TexturedModel(TerrainGenerator::generateTerrain(loader, "res/heightmap.png"),
+													Loader::loadTexture("res/blendMap.png")));
+
+	registry.emplace<Transform3DComponent>(terrain, glm::vec3{-200, -10, -200}, glm::vec3{1, 1, 1});
+	registry.emplace<TexturedComponent>(terrain, Loader::loadAllTextures(
+			{"res/dirt.png", "res/grass.jpeg", "res/rock.png", "res/flowers.png"}));
 
 	EventHandler eventHandler;
 
