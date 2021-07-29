3059fbfb8df5286579b57672c0a6db0d49a5e5ab
david <david.marcinkovic@fer.hr>
2021-07-29 19:45:49 +0200

    Implement Animation fragment shader the same way as Object fragment shader

--- engine/core/shader/sources/AnimationFragmentShader.glsl
+++ engine/core/shader/sources/AnimationFragmentShader.glsl
@@ -18,6 +18,9 @@ uniform float refractionIndex;
 
 uniform int addShadow;
 
+uniform float shineDamper;
+uniform int material;
+
 uniform int addBloom;
 uniform sampler2D bloomTexture;
 uniform float bloomStrength;
@@ -60,7 +63,7 @@ float shadowCalculation(vec4 lightSpacePosition)
 
 void main()
 {
-    vec4 textureColor = texture(objectTexture, texCoordinates);
+    vec4 textureColor = renderColor == 1 ? color : texture(objectTexture, texCoordinates);
 
     const float ambientFactor = 0.2;
     vec3 ambient = lightColor * ambientFactor;
@@ -69,7 +72,35 @@ void main()
     const float diffuseFactor = max(dot(lightDirection, surfaceNormal), 0.0);
     vec3 diffuse = lightColor * diffuseFactor;
 
-    vec3 totalColor = (diffuse + ambient) * textureColor.rgb;
+    float shadow = addShadow == 1 ? shadowCalculation(fragmentPositionInLightSpace) : 0;
 
-    outColor = vec4(totalColor, 1.0);
+    vec3 toCameraVector = normalize(cameraPosition - worldPosition);
+    vec3 reflectedVector = reflect(-lightDirection, surfaceNormal);
+
+    float specularFactor = max(dot(reflectedVector, toCameraVector), 0.0);
+    specularFactor = pow(specularFactor, material);
+
+    const float c0 = 1.0;
+    const float c1 = 0.0;
+    const float c2 = 0.001;
+
+    float distance = length(lightPosition - worldPosition);
+    float attenuation = c0 + distance * c1 + distance * distance * c2;
+    vec3 specular = specularFactor * shineDamper * lightColor / attenuation;
+
+    vec3 totalColor = (diffuse * (1- shadow) + ambient + specular) * textureColor.rgb;
+
+    vec3 R = reflect(toCameraVector, surfaceNormal);
+    vec3 reflectionColor = texture(skybox, R).rgb;
+    outColor = vec4(mix(totalColor, reflectionColor, reflectiveFactor), 1.0);
+
+    vec3 refractionVector = refract(toCameraVector, surfaceNormal, refractionIndex);
+    vec3 refractionColor = texture(skybox, refractionVector).rgb;
+
+    outColor = vec4(mix(outColor.rgb, refractionColor, refractionFactor), 1.0);
+
+    if (addBloom == 1)
+    {
+        outColor += texture(bloomTexture, texCoordinates) * bloomStrength;
+    }
 }
