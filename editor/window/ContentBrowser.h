f07ebb5b901b09412bed7a06773c4a7d5f997b8d
david <david.marcinkovic@fer.hr>
2021-08-18 21:25:55 +0200

    Created class ContentBrowser

---
+++ editor/window/ContentBrowser.h
@@ -0,0 +1,15 @@
+//
+// Created by david on 18. 08. 2021..
+//
+
+#ifndef SURVIVE_CONTENTBROWSER_H
+#define SURVIVE_CONTENTBROWSER_H
+
+
+class ContentBrowser
+{
+
+};
+
+
+#endif //SURVIVE_CONTENTBROWSER_H
