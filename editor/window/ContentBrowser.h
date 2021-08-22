2be23d6302d8cc810f2c90576296b600d21f929a
david <david.marcinkovic@fer.hr>
2021-08-22 15:03:34 +0200

    Decrease icon size

--- editor/window/ContentBrowser.h
+++ editor/window/ContentBrowser.h
@@ -16,9 +16,9 @@ namespace Survive
 	class ContentBrowser
 	{
 	private:
-		static constexpr float ICON_SIZE = 80.0f;
+		static constexpr float ICON_SIZE = 70.0f;
 		static constexpr float SPACING = 30.0f;
-		static constexpr float TEXT_WIDTH = 80.0f;
+		static constexpr float TEXT_WIDTH = 70.0f;
 
 		const ImVec2 m_Uv0, m_Uv1;
 		std::vector<File> m_DirectoryContent{};
