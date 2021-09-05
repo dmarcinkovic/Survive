9e6d7cb36ff50f5b45ded5d1dfe4c71e3975dffb
david <david.marcinkovic@fer.hr>
2021-09-05 20:58:11 +0200

    Create Inspector class

---
+++ editor/window/Inspector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 05. 09. 2021..
+//
+
+#include "Inspector.h"
---
+++ editor/window/Inspector.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 05. 09. 2021..
+//
+
+#ifndef SURVIVE_INSPECTOR_H
+#define SURVIVE_INSPECTOR_H
+
+namespace Survive
+{
+	class Inspector
+	{
+
+	};
+}
+
+#endif //SURVIVE_INSPECTOR_H
