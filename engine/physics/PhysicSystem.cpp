61cf7b664e06abe4494871d4352d0a6fc159d472
david <david.marcinkovic@fer.hr>
2021-10-13 21:11:36 +0200

    Create PhysicSystem classs

---
+++ engine/physics/PhysicSystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 13. 10. 2021..
+//
+
+#include "PhysicSystem.h"
---
+++ engine/physics/PhysicSystem.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 13. 10. 2021..
+//
+
+#ifndef SURVIVE_PHYSICSYSTEM_H
+#define SURVIVE_PHYSICSYSTEM_H
+
+namespace Survive
+{
+	class PhysicSystem
+	{
+		
+	};
+}
+
+#endif //SURVIVE_PHYSICSYSTEM_H
