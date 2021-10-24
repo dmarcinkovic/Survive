d3473f73e03665c723add8629488806598dfe6b2
david <david.marcinkovic@fer.hr>
2021-10-24 18:12:35 +0200

    Create PhysicsGizmo class

---
+++ editor/tools/gizmo/PhysicsGizmo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 24. 10. 2021..
+//
+
+#include "PhysicsGizmo.h"
---
+++ editor/tools/gizmo/PhysicsGizmo.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 24. 10. 2021..
+//
+
+#ifndef SURVIVE_PHYSICSGIZMO_H
+#define SURVIVE_PHYSICSGIZMO_H
+
+namespace Survive
+{
+	class PhysicsGizmo
+	{
+
+	};
+}
+
+#endif //SURVIVE_PHYSICSGIZMO_H
