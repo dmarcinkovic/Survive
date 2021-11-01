1e53391fb8399ba07083523598602ebf7263a23a
david <david.marcinkovic@fer.hr>
2021-11-01 10:28:54 +0100

    Created CircleGizmos class

---
+++ editor/tools/gizmo/physics/CircleGizmos.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 01. 11. 2021..
+//
+
+#include "CircleGizmos.h"
---
+++ editor/tools/gizmo/physics/CircleGizmos.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 01. 11. 2021..
+//
+
+#ifndef SURVIVE_CIRCLEGIZMOS_H
+#define SURVIVE_CIRCLEGIZMOS_H
+
+namespace Survive
+{
+	class CircleGizmos
+	{
+		
+	};
+}
+
+#endif //SURVIVE_CIRCLEGIZMOS_H
