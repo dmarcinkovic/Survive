1ec378dbe2356f914fc7c1b517e6885cd68ec28e
david <david.marcinkovic@fer.hr>
2021-11-04 20:31:18 +0100

    Created EdgeGizmos class

---
+++ editor/tools/gizmo/physics/EdgeGizmos.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 04. 11. 2021..
+//
+
+#ifndef SURVIVE_EDGEGIZMOS_H
+#define SURVIVE_EDGEGIZMOS_H
+
+namespace Survive
+{
+	class EdgeGizmos
+	{
+
+	};
+}
+
+#endif //SURVIVE_EDGEGIZMOS_H
