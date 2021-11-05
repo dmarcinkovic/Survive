182c8a1bd1b4cd8dc1d10375cf95d93b1f48cdb0
david <david.marcinkovic@fer.hr>
2021-11-06 00:40:53 +0100

    Implement common functions in base class

--- editor/tools/gizmo/physics/ColliderGizmos.cpp
+++ editor/tools/gizmo/physics/ColliderGizmos.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "ColliderGizmos.h"
+
+void Survive::ColliderGizmos::draw(entt::registry &registry, const Camera &camera, entt::entity selectedEntity)
+{
+
+}
+
+void Survive::ColliderGizmos::handleKeyEvents(const EventHandler &eventHandler)
+{
+	if (eventHandler.isKeyPressed(Key::A))
+	{
+		m_GizmoEnabled = true;
+	} else if (eventHandler.isKeyPressed(Key::ESCAPE))
+	{
+		m_GizmoEnabled = false;
+	}
+}
+
+void Survive::ColliderGizmos::setRect(float x, float y, float width, float height)
+{
+	m_X = x;
+	m_Y = y;
+	m_Width = width;
+	m_Height = height;
+}
--- editor/tools/gizmo/physics/ColliderGizmos.h
+++ editor/tools/gizmo/physics/ColliderGizmos.h
@@ -5,11 +5,30 @@
 #ifndef SURVIVE_COLLIDERGIZMOS_H
 #define SURVIVE_COLLIDERGIZMOS_H
 
+#include <imgui.h>
+
+#include "entt.hpp"
+#include "Components.h"
+#include "EventHandler.h"
+#include "Camera.h"
+
 namespace Survive
 {
 	class ColliderGizmos
 	{
-		
+	protected:
+		float m_X{}, m_Y{}, m_Width{}, m_Height{};
+
+		bool m_GizmoEnabled{};
+
+		bool m_IsUsing{};
+
+	public:
+		virtual void draw(entt::registry &registry, const Camera &camera, entt::entity selectedEntity);
+
+		void handleKeyEvents(const EventHandler &eventHandler);
+
+		void setRect(float x, float y, float width, float height);
 	};
 }
 
