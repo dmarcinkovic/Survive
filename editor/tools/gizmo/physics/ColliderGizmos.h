799f256c5b97b24dc849b0ea7484826ea859f565
david <david.marcinkovic@fer.hr>
2021-11-06 00:34:16 +0100

    Create base class for all collider gizmos

---
+++ editor/tools/gizmo/physics/ColliderGizmos.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 06. 11. 2021..
+//
+
+#ifndef SURVIVE_COLLIDERGIZMOS_H
+#define SURVIVE_COLLIDERGIZMOS_H
+
+namespace Survive
+{
+	class ColliderGizmos
+	{
+		
+	};
+}
+
+#endif //SURVIVE_COLLIDERGIZMOS_H
