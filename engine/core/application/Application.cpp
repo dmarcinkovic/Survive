c9acfdd13a62bbd19965ce21cc280d3f26a016d4
david <david.marcinkovic@fer.hr>
2021-11-09 00:26:09 +0100

    Add Tag components to entites

--- engine/core/application/Application.cpp
+++ engine/core/application/Application.cpp
@@ -10,12 +10,14 @@ Survive::Application::Application(int windowWidth, int windowHeight, const char 
 		  m_Renderer(m_Light), m_Editor(m_Renderer), m_World(std::make_unique<b2World>(m_Gravity))
 {
 	auto cube = m_Registry.create();
+	m_Registry.emplace<TagComponent>(cube, "cube");
 	m_Registry.emplace<Transform3DComponent>(cube, glm::vec3{0, 2, -10}, glm::vec3{1.0f}, glm::vec3{0, 30, 0});
 	m_Registry.emplace<RigidBodyComponent>(cube, false);
 	m_Registry.emplace<Render3DComponent>(cube, TexturedModel(ObjParser::loadObj("res/cube.obj", m_Loader), Texture()));
 	m_Registry.emplace<SpriteComponent>(cube, glm::vec4{0.8f, 0.3f, 0.1f, 1.0f});
 
 	auto ground = m_Registry.create();
+	m_Registry.emplace<TagComponent>(ground, "ground");
 	m_Registry.emplace<Transform3DComponent>(ground, glm::vec3{0, -2, -10}, glm::vec3{6, 0.5f, 8});
 	m_Registry.emplace<RigidBodyComponent>(ground, false);
 	m_Registry.emplace<Render3DComponent>(ground, TexturedModel(ObjParser::loadObj("res/cube.obj", m_Loader), Texture()));
