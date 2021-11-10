d99f924688b4e2892c55cb3fb075b6d811fc6222
david <david.marcinkovic@fer.hr>
2021-11-10 19:15:46 +0100

    Add all necessary data into RigidBody3D component

--- engine/components/RigidBody3DComponent.h
+++ engine/components/RigidBody3DComponent.h
@@ -5,11 +5,31 @@
 #ifndef SURVIVE_RIGIDBODY3DCOMPONENT_H
 #define SURVIVE_RIGIDBODY3DCOMPONENT_H
 
+#include <reactphysics3d/reactphysics3d.h>
+
 namespace Survive
 {
 	struct RigidBody3DComponent
 	{
+		rp3d::RigidBody *body{};
 
+		rp3d::BodyType bodyType = rp3d::BodyType::STATIC;
+		float mass{};
+		float linearDamping{};
+		rp3d::Vector3 linearVelocity = rp3d::Vector3{0.0f, 0.0f, 0.0f};
+		float angularDrag{};
+		bool useGravity = true;
+
+		RigidBody3DComponent() = default;
+
+		RigidBody3DComponent(rp3d::BodyType bodyType, float mass, float linearDamping = 0.0f,
+							 const rp3d::Vector3 &linearVelocity = rp3d::Vector3{0.0f, 0.0f, 0.0f},
+							 float angularDrag = 0.0f,
+							 bool useGravity = true)
+				: bodyType(bodyType), mass(mass), linearDamping(linearDamping),
+				  linearVelocity(linearVelocity), angularDrag(angularDrag), useGravity(useGravity)
+		{
+		}
 	};
 }
 
