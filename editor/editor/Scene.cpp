d7076a25039d105cd27a5db854cf8ee21923896e
david <david.marcinkovic@fer.hr>
2021-11-26 00:56:20 +0100

    Created Scene class

---
+++ editor/editor/Scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 26. 11. 2021..
+//
+
+#include "Scene.h"
---
+++ editor/editor/Scene.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 26. 11. 2021..
+//
+
+#ifndef SURVIVE_SCENE_H
+#define SURVIVE_SCENE_H
+
+namespace Survive
+{
+	class Scene
+	{
+
+	};
+}
+
+#endif //SURVIVE_SCENE_H
