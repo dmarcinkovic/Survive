4836dde9a5c116dd022ed021490de794285df702
david <david.marcinkovic@fer.hr>
2021-11-26 22:03:59 +0100

    Move PropertyWindow enum to separate header

---
+++ editor/window/PropertyWindow.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 26. 11. 2021..
+//
+
+#ifndef SURVIVE_PROPERTYWINDOW_H
+#define SURVIVE_PROPERTYWINDOW_H
+
+namespace Survive
+{
+	enum class PropertyWindow
+	{
+		ENTITY, CAMERA, NONE
+	};
+}
+
+#endif //SURVIVE_PROPERTYWINDOW_H
