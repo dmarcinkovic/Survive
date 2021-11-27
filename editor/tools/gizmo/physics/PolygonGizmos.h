b347ee50a64e95f97e6557bc3b237881bb161614
david <david.marcinkovic@fer.hr>
2021-11-28 00:39:49 +0100

    Created PolygonGizmos class

---
+++ editor/tools/gizmo/physics/PolygonGizmos.h
@@ -0,0 +1,19 @@
+//
+// Created by david on 28. 11. 2021..
+//
+
+#ifndef SURVIVE_POLYGONGIZMOS_H
+#define SURVIVE_POLYGONGIZMOS_H
+
+#include "ColliderGizmos.h"
+
+namespace Survive
+{
+	class PolygonGizmos : public ColliderGizmos
+	{
+
+	};
+}
+
+
+#endif //SURVIVE_POLYGONGIZMOS_H
