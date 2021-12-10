3b59852b1f1ac098c8c784f2764f72efd8b91576
david <david.marcinkovic@fer.hr>
2021-12-10 21:29:43 +0100

    Draw rectangle

--- engine/core/application/Application.cpp
+++ engine/core/application/Application.cpp
@@ -10,17 +10,14 @@ Survive::Application::Application(int windowWidth, int windowHeight, const char 
 		: m_Display(windowWidth, windowHeight, title), m_Light(glm::vec3{100.0f}, glm::vec3{1.0f}),
 		  m_Renderer(m_Light), m_Editor(m_Renderer), m_World(std::make_unique<b2World>(m_Gravity))
 {
-	auto polygon = m_Registry.create();
-	m_Registry.emplace<TagComponent>(polygon, "polygon");
-	m_Registry.emplace<Transform3DComponent>(polygon, glm::vec3{0, 0.7f, 0}, glm::vec3{0.25f, 0.25f, 1.0f});
-	m_Registry.emplace<Render2DComponent>(polygon,
+	auto box = m_Registry.create();
+	m_Registry.emplace<TagComponent>(box, "box");
+	m_Registry.emplace<Transform3DComponent>(box, glm::vec3{0, 0.7f, 0}, glm::vec3{0.25f, 0.25f, 1.0f});
+	m_Registry.emplace<Render2DComponent>(box,
 										  TexturedModel(m_Loader.renderQuad(),
-														m_Loader.loadTexture("assets/textures/polygon.png")));
-	m_Registry.emplace<RigidBody2DComponent>(polygon, b2_dynamicBody);
-	m_Registry.emplace<PolygonCollider2DComponent>(polygon,
-												   std::vector<b2Vec2>{b2Vec2(-2.5, 0.5), b2Vec2(-1.4, -2.5),
-																	   b2Vec2(1.4, -2.5), b2Vec2(2.5, 0.5),
-																	   b2Vec2(0, 2.5)}, 1.0f, 0.3f, 0.5f);
+														m_Loader.loadTexture("assets/textures/rectangle.png")));
+	m_Registry.emplace<RigidBody2DComponent>(box, b2_dynamicBody);
+	m_Registry.emplace<BoxCollider2DComponent>(box, 2.5f, 2.5f, 1.0f, 0.3f, 0.5f);
 
 	auto ground = m_Registry.create();
 	m_Registry.emplace<TagComponent>(ground, "ground");
