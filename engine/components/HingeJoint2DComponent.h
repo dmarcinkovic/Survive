ff001303213cfb6da67bd3c2ebe7257b5bf3d16a
david <david.marcinkovic@fer.hr>
2021-12-18 21:15:56 +0100

    Create HingeJoint2D component constructor

--- engine/components/HingeJoint2DComponent.h
+++ engine/components/HingeJoint2DComponent.h
@@ -5,11 +5,32 @@
 #ifndef SURVIVE_HINGEJOINT2DCOMPONENT_H
 #define SURVIVE_HINGEJOINT2DCOMPONENT_H
 
+#include <box2d/box2d.h>
+#include <glm/glm.hpp>
+
 namespace Survive
 {
 	struct HingeJoint2DComponent
 	{
+		b2RevoluteJointDef jointDef{};
+		b2Body *connectedBody{};
 
+		HingeJoint2DComponent() = default;
+
+		HingeJoint2DComponent(b2Body *connectedBody, const b2Vec2 &anchor, const b2Vec2 &connectedAnchor,
+							  bool useMotor = false, float motorSpeed = 0, float maxTorque = 0, bool useLimits = false,
+							  float lowerAngle = 0, float upperAngle = 0)
+		{
+			jointDef.bodyB = connectedBody;
+			jointDef.localAnchorA = anchor;
+			jointDef.localAnchorB = connectedAnchor;
+			jointDef.enableMotor = useMotor;
+			jointDef.motorSpeed = motorSpeed;
+			jointDef.maxMotorTorque = maxTorque;
+			jointDef.enableLimit = useLimits;
+			jointDef.lowerAngle = glm::radians(lowerAngle);
+			jointDef.upperAngle = glm::radians(upperAngle);
+		}
 	};
 }
 
