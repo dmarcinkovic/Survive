62ece944e111996348c1262feaab846fdcdfbf3f
david <david.marcinkovic@fer.hr>
2021-12-27 10:38:34 +0100

    Created SaveDialog class

---
+++ editor/tools/fileChooser/SaveDialog.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 27. 12. 2021..
+//
+
+#ifndef SURVIVE_SAVEDIALOG_H
+#define SURVIVE_SAVEDIALOG_H
+
+namespace Survive
+{
+	class SaveDialog
+	{
+
+	};
+}
+
+#endif //SURVIVE_SAVEDIALOG_H
