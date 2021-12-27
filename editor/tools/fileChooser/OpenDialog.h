b049c7ba08a9bedb3fa367b48cda00bb19d7c932
david <david.marcinkovic@fer.hr>
2021-12-27 10:39:10 +0100

    Create OpenDialog class

---
+++ editor/tools/fileChooser/OpenDialog.h
@@ -0,0 +1,17 @@
+//
+// Created by david on 27. 12. 2021..
+//
+
+#ifndef SURVIVE_OPENDIALOG_H
+#define SURVIVE_OPENDIALOG_H
+
+namespace Survive
+{
+	class OpenDialog
+	{
+
+	};
+
+}
+
+#endif //SURVIVE_OPENDIALOG_H
