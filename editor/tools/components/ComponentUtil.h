3dc026195b694852b52aa25311d129d6b7ff80b7
david <david.marcinkovic@fer.hr>
2022-01-22 22:54:18 +0100

    Add DistanceJoint to editor

--- editor/tools/components/ComponentUtil.h
+++ editor/tools/components/ComponentUtil.h
@@ -28,10 +28,11 @@ namespace Survive
 		{
 			std::vector<const char *> components = {
 					"Animation component", "Bloom component", "Box Collider2D component", "Circle Collider2D component",
-					"Edge Collider2D component", "Hinge Joint2D component", "PolygonCollider2D component",
-					"Reflection component", "Refraction component", "Render2D Component", "Render3D component",
-					"RigidBody2D component", "Rigid body component", "Shadow component", "Sound component",
-					"Sprite component", "Sprite sheet component", "Text Component", "Transform3D component"
+					"Distance Joint2D component", "Edge Collider2D component", "Hinge Joint2D component",
+					"PolygonCollider2D component", "Reflection component", "Refraction component", "Render2D Component",
+					"Render3D component", "RigidBody2D component", "Rigid body component", "Shadow component",
+					"Sound component", "Sprite component", "Sprite sheet component", "Text Component",
+					"Transform3D component"
 			};
 
 			return components;
@@ -43,6 +44,7 @@ namespace Survive
 			drawComponent<BloomComponent>(registry, entity);
 			drawComponent<BoxCollider2DComponent>(registry, entity);
 			drawComponent<CircleCollider2DComponent>(registry, entity);
+			drawComponent<DistanceJoint2DComponent>(registry, entity);
 			drawComponent<EdgeCollider2DComponent>(registry, entity);
 			drawComponent<HingeJoint2DComponent>(registry, entity);
 			drawComponent<PolygonCollider2DComponent>(registry, entity);
@@ -73,34 +75,36 @@ namespace Survive
 				case 3:
 					return addComponent<CircleCollider2DComponent>(registry, entity);
 				case 4:
-					return addComponent<EdgeCollider2DComponent>(registry, entity);
+					return addComponent<DistanceJoint2DComponent>(registry, entity);
 				case 5:
-					return addComponent<HingeJoint2DComponent>(registry, entity);
+					return addComponent<EdgeCollider2DComponent>(registry, entity);
 				case 6:
-					return addComponent<PolygonCollider2DComponent>(registry, entity);
+					return addComponent<HingeJoint2DComponent>(registry, entity);
 				case 7:
-					return addComponent<ReflectionComponent>(registry, entity);
+					return addComponent<PolygonCollider2DComponent>(registry, entity);
 				case 8:
-					return addComponent<RefractionComponent>(registry, entity);
+					return addComponent<ReflectionComponent>(registry, entity);
 				case 9:
-					return addComponent<Render2DComponent>(registry, entity);
+					return addComponent<RefractionComponent>(registry, entity);
 				case 10:
-					return addComponent<Render3DComponent>(registry, entity);
+					return addComponent<Render2DComponent>(registry, entity);
 				case 11:
-					return addComponent<RigidBody2DComponent>(registry, entity);
+					return addComponent<Render3DComponent>(registry, entity);
 				case 12:
-					return addComponent<RigidBodyComponent>(registry, entity);
+					return addComponent<RigidBody2DComponent>(registry, entity);
 				case 13:
-					return addComponent<ShadowComponent>(registry, entity);
+					return addComponent<RigidBodyComponent>(registry, entity);
 				case 14:
-					return addComponent<SoundComponent>(registry, entity);
+					return addComponent<ShadowComponent>(registry, entity);
 				case 15:
-					return addComponent<SpriteComponent>(registry, entity);
+					return addComponent<SoundComponent>(registry, entity);
 				case 16:
-					return addComponent<SpriteSheetComponent>(registry, entity);
+					return addComponent<SpriteComponent>(registry, entity);
 				case 17:
-					return addComponent<TextComponent>(registry, entity);
+					return addComponent<SpriteSheetComponent>(registry, entity);
 				case 18:
+					return addComponent<TextComponent>(registry, entity);
+				case 19:
 					return addComponent<Transform3DComponent>(registry, entity);
 				default:
 					throw std::runtime_error("Selected item not implemented");
