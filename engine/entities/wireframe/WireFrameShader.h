f6104de7f91712f26d0e22c3eb08dc1ae020176b
david <david.marcinkovic@fer.hr>
2022-01-24 20:45:34 +0100

    Create WireFrameShader class

---
+++ engine/entities/wireframe/WireFrameShader.h
@@ -0,0 +1,18 @@
+//
+// Created by david on 24. 01. 2022..
+//
+
+#ifndef SURVIVE_WIREFRAMESHADER_H
+#define SURVIVE_WIREFRAMESHADER_H
+
+#include "Shader.h"
+
+namespace Survive
+{
+	class WireFrameShader : public Shader
+	{
+
+	};
+}
+
+#endif //SURVIVE_WIREFRAMESHADER_H
