6d0a586531197860c57106f702505d4a2cc995f1
david <david.marcinkovic@fer.hr>
2022-02-12 23:07:24 +0100

    Draw anchors

--- editor/tools/gizmo/physics/joints/HingeJointGizmos.cpp
+++ editor/tools/gizmo/physics/joints/HingeJointGizmos.cpp
@@ -2,4 +2,89 @@
 // Created by david on 24. 01. 2022..
 //
 
+#include <iostream>
+
 #include "HingeJointGizmos.h"
+#include "Maths.h"
+#include "Constants.h"
+#include "Util.h"
+
+void Survive::HingeJointGizmos::draw(entt::registry &registry, const Camera &camera, entt::entity selectedEntity)
+{
+	if (selectedEntity == entt::null)
+	{
+		m_GizmoEnabled = false;
+	}
+
+	if (selectedEntity != entt::null && registry.all_of<HingeJoint2DComponent, Transform3DComponent>(selectedEntity))
+	{
+		if (m_GizmoEnabled)
+		{
+			m_IsUsing = ImGui::IsMouseDragging(ImGuiMouseButton_Left);
+
+			drawGizmos(registry, selectedEntity, camera);
+		}
+	}
+}
+
+bool Survive::HingeJointGizmos::isOver()
+{
+	return false;
+}
+
+void Survive::HingeJointGizmos::handleKeyEvents(const Survive::EventHandler &eventHandler)
+{
+	if (eventHandler.isKeyPressed(Key::D))
+	{
+		m_GizmoEnabled = true;
+	} else if (eventHandler.isKeyPressed(Key::ESCAPE))
+	{
+		m_GizmoEnabled = false;
+	}
+}
+
+void Survive::HingeJointGizmos::drawGizmos(entt::registry &registry, entt::entity bodyA, const Camera &camera)
+{
+	HingeJoint2DComponent &hingeComponent = registry.get<HingeJoint2DComponent>(bodyA);
+	const b2RevoluteJointDef &jointDef = hingeComponent.jointDef;
+
+	drawAnchorA(registry, bodyA, camera, jointDef.localAnchorA);
+	drawAnchorB(registry, hingeComponent.connectedBody, camera, jointDef.localAnchorB);
+}
+
+void Survive::HingeJointGizmos::drawAnchorA(entt::registry &registry, entt::entity bodyA, const Camera &camera,
+											const b2Vec2 &anchor)
+{
+	ImVec2 anchorPosition = getAnchorPosition(registry, bodyA, camera, anchor);
+
+	ImDrawList *drawList = ImGui::GetWindowDrawList();
+	drawList->AddCircle(anchorPosition, 7, IM_COL32(0, 0, 230, 255), 0, 3.0f);
+}
+
+void Survive::HingeJointGizmos::drawAnchorB(entt::registry &registry, entt::entity bodyB, const Camera &camera,
+											const b2Vec2 &anchor)
+{
+	ImVec2 anchorPosition;
+
+	if (bodyB == entt::null)
+	{
+		anchorPosition = getPoint(glm::vec3{0.0f}, anchor, camera, glm::mat4{1.0f}, 0.0f);
+	} else
+	{
+		anchorPosition = getAnchorPosition(registry, bodyB, camera, anchor);
+	}
+
+	ImDrawList *drawList = ImGui::GetWindowDrawList();
+	drawList->AddCircleFilled(anchorPosition, 5, IM_COL32(0, 0, 200, 255));
+}
+
+ImVec2 Survive::HingeJointGizmos::getAnchorPosition(entt::registry &registry, entt::entity body, const Camera &camera,
+													const b2Vec2 &anchor)
+{
+	const Transform3DComponent &transform = registry.get<Transform3DComponent>(body);
+	glm::mat4 modelMatrix = Maths::createTransformationMatrix(transform.position);
+
+	float angle = glm::radians(transform.rotation.z);
+
+	return getPoint(transform.position, anchor, camera, modelMatrix, angle);
+}
--- editor/tools/gizmo/physics/joints/HingeJointGizmos.h
+++ editor/tools/gizmo/physics/joints/HingeJointGizmos.h
@@ -5,11 +5,29 @@
 #ifndef SURVIVE_HINGEJOINTGIZMOS_H
 #define SURVIVE_HINGEJOINTGIZMOS_H
 
+#include "PhysicsGizmosBase.h"
+#include "Components.h"
+
 namespace Survive
 {
-	class HingeJointGizmos
+	class HingeJointGizmos : public PhysicsGizmosBase
 	{
+	public:
+		void draw(entt::registry &registry, const Camera &camera, entt::entity selectedEntity) override;
 
+		static bool isOver();
+
+		void handleKeyEvents(const EventHandler &eventHandler) override;
+
+	private:
+		void drawGizmos(entt::registry &registry, entt::entity bodyA, const Camera &camera);
+
+		void drawAnchorA(entt::registry &registry, entt::entity bodyA, const Camera &camera, const b2Vec2 &anchor);
+
+		void drawAnchorB(entt::registry &registry, entt::entity bodyB, const Camera &camera, const b2Vec2 &anchor);
+
+		ImVec2
+		getAnchorPosition(entt::registry &registry, entt::entity body, const Camera &camera, const b2Vec2 &anchor);
 	};
 }
 
