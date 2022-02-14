085ce6408b41e45250fd8a5ad35a01c7d993e2ae
david <david.marcinkovic@fer.hr>
2022-02-14 19:46:04 +0100

    Created DistanceJoint gizmos

---
+++ editor/tools/gizmo/physics/joints/DistanceJointGizmos.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 14. 02. 2022..
+//
+
+#ifndef SURVIVE_DISTANCEJOINTGIZMOS_H
+#define SURVIVE_DISTANCEJOINTGIZMOS_H
+
+namespace Survive
+{
+	class DistanceJointGizmos
+	{
+
+	};
+}
+
+#endif //SURVIVE_DISTANCEJOINTGIZMOS_H
