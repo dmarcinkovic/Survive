3b7e066a6c228b1e8c99c66bd77db06dbd146edb
david <david.marcinkovic@fer.hr>
2022-02-17 21:02:13 +0100

    Take tangents as an input to vertex shader

--- engine/core/shader/sources/ObjectVertexShader.glsl
+++ engine/core/shader/sources/ObjectVertexShader.glsl
@@ -3,6 +3,7 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec2 textureCoordinates;
 layout (location = 2) in vec3 normal;
+layout (location = 3) in vec3 tangent;
 
 out vec3 surfaceNormal;
 
