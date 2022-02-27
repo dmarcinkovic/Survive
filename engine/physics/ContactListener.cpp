c0ea9fb399c637565c81029f4cd7d4b7f1289901
david <david.marcinkovic@fer.hr>
2022-02-28 00:00:11 +0100

    Created contact listener class

---
+++ engine/physics/ContactListener.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 27. 02. 2022..
+//
+
+#include "ContactListener.h"
---
+++ engine/physics/ContactListener.h
@@ -0,0 +1,18 @@
+//
+// Created by david on 27. 02. 2022..
+//
+
+#ifndef SURVIVE_CONTACTLISTENER_H
+#define SURVIVE_CONTACTLISTENER_H
+
+#include <box2d/box2d.h>
+
+namespace Survive
+{
+	class ContactListener
+	{
+
+	};
+}
+
+#endif //SURVIVE_CONTACTLISTENER_H
