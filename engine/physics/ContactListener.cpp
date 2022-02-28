67f6333249d4be6a7563cfa4cd8c348a03c6bcbb
david <david.marcinkovic@fer.hr>
2022-02-28 23:48:47 +0100

    Create contact listener that calls callbacks from script component

--- engine/physics/ContactListener.cpp
+++ engine/physics/ContactListener.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "ContactListener.h"
+#include "Components.h"
+#include "entt.hpp"
+
+Survive::ContactListener::ContactListener(entt::registry &registry)
+	: m_Registry(registry)
+{
+
+}
+
+void Survive::ContactListener::BeginContact(b2Contact *contact)
+{
+	auto[entityA, entityB] = getBodies(contact);
+
+	if (m_Registry.any_of<ScriptComponent>(entityA))
+	{
+		ScriptComponent &script = m_Registry.get<ScriptComponent>(entityA);
+
+		if (script.script != nullptr)
+		{
+			script.script->onCollisionEnter(entityB);
+		}
+	}
+}
+
+void Survive::ContactListener::EndContact(b2Contact *contact)
+{
+	auto[entityA, entityB] = getBodies(contact);
+
+	if (m_Registry.any_of<ScriptComponent>(entityA))
+	{
+		ScriptComponent &script = m_Registry.get<ScriptComponent>(entityA);
+
+		if (script.script != nullptr)
+		{
+			script.script->onCollisionExit(entityB);
+		}
+	}
+}
+
+std::pair<entt::entity, entt::entity> Survive::ContactListener::getBodies(b2Contact *contact)
+{
+	auto bodyA = contact->GetFixtureA()->GetUserData().pointer;
+	auto entityA = static_cast<entt::entity>(bodyA);
+
+	auto bodyB = contact->GetFixtureB()->GetUserData().pointer;
+	auto entityB = static_cast<entt::entity>(bodyB);
+
+	return {entityA, entityB};
+}
--- engine/physics/ContactListener.h
+++ engine/physics/ContactListener.h
@@ -6,12 +6,26 @@
 #define SURVIVE_CONTACTLISTENER_H
 
 #include <box2d/box2d.h>
+#include <functional>
+
+#include "entt.hpp"
 
 namespace Survive
 {
-	class ContactListener
+	class ContactListener : public b2ContactListener
 	{
+	private:
+		entt::registry &m_Registry;
 
+	public:
+		explicit ContactListener(entt::registry &registry);
+
+		void BeginContact(b2Contact *contact) override;
+
+		void EndContact(b2Contact *contact) override;
+
+	private:
+		static std::pair<entt::entity, entt::entity> getBodies(b2Contact *contact);
 	};
 }
 
