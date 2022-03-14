e25ffdf63d5c21f326be324b73da5e44977a2ce7
david <david.marcinkovic@fer.hr>
2022-03-14 21:06:28 +0100

    Created EditorEventHandler class

---
+++ editor/editor/EditorEventHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by david on 14. 03. 2022..
+//
+
+#include "EditorEventHandler.h"
---
+++ editor/editor/EditorEventHandler.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 14. 03. 2022..
+//
+
+#ifndef SURVIVE_EDITOREVENTHANDLER_H
+#define SURVIVE_EDITOREVENTHANDLER_H
+
+namespace Survive
+{
+	class EditorEventHandler
+	{
+		
+	};
+}
+
+#endif //SURVIVE_EDITOREVENTHANDLER_H
