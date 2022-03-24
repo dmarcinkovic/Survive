b5c70146cc35f28b3f2170d1e7d38ae91be4e672
david <david.marcinkovic@fer.hr>
2022-03-24 20:52:38 +0100

    Remove libtbb dependency

--- engine/core/renderer/Loader.cpp
+++ engine/core/renderer/Loader.cpp
@@ -2,7 +2,7 @@
 // Created by david on 24. 03. 2020..
 //
 
-#include <execution>
+#include <stdexcept>
 
 #include "Loader.h"
 #include "stb_image.h"
@@ -277,7 +277,7 @@ Survive::Loader::loadImages(const std::vector<const char *> &textures)
 {
 	std::unordered_map<const char *, std::tuple<std::uint8_t *, int, int>> images;
 
-	std::for_each(std::execution::seq, textures.begin(), textures.end(), [&](const char *filename) {
+	std::for_each(textures.begin(), textures.end(), [&](const char *filename) {
 		int width, height, BPP;
 		std::uint8_t *image = stbi_load(filename, &width, &height, &BPP, 4);
 
