3678f5b8ff53632c0ba2f7233fdf47f4ab5abb3e
david <david.marcinkovic@fer.hr>
2022-04-22 23:54:38 +0200

    Create default constructor for HingeJoint3D component

--- engine/components/HingeJoint3DComponent.h
+++ engine/components/HingeJoint3DComponent.h
@@ -5,11 +5,21 @@
 #ifndef SURVIVE_HINGEJOINT3DCOMPONENT_H
 #define SURVIVE_HINGEJOINT3DCOMPONENT_H
 
+#include <reactphysics3d/reactphysics3d.h>
+
+#include "entt.hpp"
+
 namespace Survive
 {
 	struct HingeJoint3DComponent
 	{
+		rp3d::HingeJointInfo jointInfo;
+		entt::entity connectedBody = entt::null;
+		std::string connectedBodyName = "none";
 
+		HingeJoint3DComponent()
+			: jointInfo(nullptr, nullptr, rp3d::Vector3{}, rp3d::Vector3{})
+		{}
 	};
 }
 
