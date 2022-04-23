20f265c7c23cdf1bfdc36bd00c44fe7532e5d6fe
david <david.marcinkovic@fer.hr>
2022-04-23 11:10:00 +0200

    Finish hinge joint 3D component template

--- editor/tools/components/templates/HingeJoint3DComponentTemplate.h
+++ editor/tools/components/templates/HingeJoint3DComponentTemplate.h
@@ -9,11 +9,12 @@
 
 #include "Components.h"
 #include "ComponentTemplate.h"
+#include "JointComponentTemplate.h"
 
 namespace Survive
 {
 	template<>
-	class ComponentTemplate<HingeJoint3DComponent>
+	class ComponentTemplate<HingeJoint3DComponent> : public JointComponentTemplate
 	{
 	public:
 		static void drawComponent(HingeJoint3DComponent &component, bool *visible)
@@ -22,8 +23,93 @@ namespace Survive
 
 			if (ImGui::CollapsingHeader("Hinge joint 3D", visible))
 			{
+				ImGui::Columns(2, nullptr, false);
 
+				drawConnectedBodyUI(component);
+
+				rp3d::HingeJointInfo &jointInfo = component.jointInfo;
+
+				drawFirstBodyInfo(jointInfo);
+				drawSecondBodyInfo(jointInfo);
+
+				drawMotorProperties(jointInfo);
+				drawLimitProperties(jointInfo);
+
+				EditorUtil::drawColumnInputBool("Enable Collision", "##Hinge3DCollision", jointInfo.isCollisionEnabled);
+
+				ImGui::Columns();
 			}
+		}
+
+	private:
+		static void drawConnectedBodyUI(HingeJoint3DComponent &component)
+		{
+			std::string &connectedBodyName = component.connectedBodyName;
+			EditorUtil::drawColumnInputText("##Hinge3DJoint", "Connected Rigid Body", connectedBodyName);
+			initializeDragDropTarget(component.connectedBody, connectedBodyName);
+
+			ImGui::NextColumn();
+		}
+
+		static void drawFirstBodyInfo(rp3d::HingeJointInfo &jointInfo)
+		{
+			if (jointInfo.isUsingLocalSpaceAnchors)
+			{
+				EditorUtil::drawColumnDragFloat3("Anchor", "##Hinge3DAnchor", jointInfo.anchorPointBody1LocalSpace);
+				EditorUtil::drawColumnDragFloat3("Axis", "##Hinge3DAxis", jointInfo.rotationAxisBody1Local);
+			} else
+			{
+				EditorUtil::drawColumnDragFloat3("Anchor", "##Hinge3DAnchor", jointInfo.anchorPointWorldSpace);
+				EditorUtil::drawColumnDragFloat3("Axis", "##Hinge3DAxis", jointInfo.rotationAxisWorld);
+			}
+		}
+
+		static void drawSecondBodyInfo(rp3d::HingeJointInfo &jointInfo)
+		{
+			EditorUtil::drawColumnInputBool("Auto Configure Connected", "##Hinge3DConfigure",
+											jointInfo.isUsingLocalSpaceAnchors);
+
+			EditorUtil::drawColumnDragFloat3("Connected Anchor", "##Hinge3DCAnchor",
+											 jointInfo.anchorPointBody2LocalSpace);
+		}
+
+		static void drawMotorProperties(rp3d::HingeJointInfo &jointInfo)
+		{
+			static constexpr float minValue = std::numeric_limits<float>::lowest();
+
+			ImGui::Separator();
+			EditorUtil::drawColumnInputBool("Use Motor", "##Hinge3DMotor", jointInfo.isMotorEnabled);
+
+			ImGui::Indent();
+
+			EditorUtil::drawColumnDragFloat("Motor Speed", "##Hinge3DMotorSpeed", jointInfo.motorSpeed, minValue);
+			EditorUtil::drawColumnDragFloat("Max Torque", "##Hinge3DTorque", jointInfo.maxMotorTorque);
+
+			ImGui::Unindent();
+		}
+
+		static void drawLimitProperties(rp3d::HingeJointInfo &jointInfo)
+		{
+			static constexpr float minValue = std::numeric_limits<float>::lowest();
+
+			ImGui::Separator();
+			EditorUtil::drawColumnInputBool("Use Limits", "##Hinge3DLimits", jointInfo.isLimitEnabled);
+
+			ImGui::Indent();
+
+			float minAngle = glm::degrees(jointInfo.minAngleLimit);
+			if (EditorUtil::drawColumnDragFloat("Min Angle", "##Hinge3DMinAngle", minAngle, -360, 0))
+			{
+				jointInfo.minAngleLimit = glm::radians(minAngle);
+			}
+
+			float maxAngle = glm::degrees(jointInfo.maxAngleLimit);
+			if (EditorUtil::drawColumnDragFloat("Max Angle", "##Hinge3DMaxAngle", maxAngle, 0, 360))
+			{
+				jointInfo.maxAngleLimit = glm::radians(maxAngle);
+			}
+
+			ImGui::Unindent();
 		}
 	};
 }
