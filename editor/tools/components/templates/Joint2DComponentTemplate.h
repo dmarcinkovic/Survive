bc1a6ed2bc4bd65afdeae84dd22bf4e42f9411e4
david <david.marcinkovic@fer.hr>
2022-04-23 11:08:48 +0200

    Move initializeDragDropTarget to base class

--- editor/tools/components/templates/Joint2DComponentTemplate.h
+++ editor/tools/components/templates/Joint2DComponentTemplate.h
@@ -8,50 +8,27 @@
 #include <imgui.h>
 #include <box2d/box2d.h>
 
+#include "JointComponentTemplate.h"
 #include "entt.hpp"
 #include "EditorUtil.h"
 #include "Log.h"
 
 namespace Survive
 {
-	class Joint2DComponentTemplate
+	class Joint2DComponentTemplate : public JointComponentTemplate
 	{
-	private:
-		static void initializeDragDropTarget(entt::entity &connectedBody, std::string &name)
-		{
-			if (ImGui::BeginDragDropTarget())
-			{
-				if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("Joint2D"))
-				{
-					auto *data = reinterpret_cast<std::tuple<int, int, const char *> *>(payload->Data);
-
-					int bodyA = std::get<0>(*data);
-					int bodyB = std::get<1>(*data);
-
-					if (bodyA == bodyB)
-					{
-						Log::logMessage(LogType::ERROR, "Body A should not be equal to body B");
-					} else
-					{
-						connectedBody = static_cast<entt::entity>(bodyB);
-						name = std::get<2>(*data);
-					}
-				}
-
-				ImGui::EndDragDropTarget();
-			}
-		}
-
 	protected:
 		static void
 		drawJoint2DProperties(const char *label, std::string &connectedBodyName, entt::entity &connectedBody,
 							  b2Vec2 &anchorA, b2Vec2 &anchorB, bool &collideConnected)
 		{
+			// TODO push ID
 			EditorUtil::drawColumnInputText(label, "Connected Rigid Body", connectedBodyName);
 			initializeDragDropTarget(connectedBody, connectedBodyName);
 
 			ImGui::NextColumn();
 
+			// TODO this label should not be distance
 			EditorUtil::drawColumnDragFloat2("Anchor", "##DistanceAnchorA", anchorA);
 			EditorUtil::drawColumnDragFloat2("Connected anchor", "##DistanceAnchorB", anchorB);
 			EditorUtil::drawColumnInputBool("Collide connected", "##DistanceCollide", collideConnected);
