fd591abdb8396eecc5ac223444800020a9c53c9d
david <david.marcinkovic@fer.hr>
2022-05-21 00:30:56 +0200

    Create Particle component

---
+++ engine/components/ParticleComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by david on 21.05.22..
+//
+
+#ifndef SURVIVE_PARTICLECOMPONENT_H
+#define SURVIVE_PARTICLECOMPONENT_H
+
+namespace Survive
+{
+	struct ParticleComponent
+	{
+
+	};
+}
+
+#endif //SURVIVE_PARTICLECOMPONENT_H
